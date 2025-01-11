46938e837d483170f99266676b103821d30c6ddf
Baek Jiung <zxcwldnd@gmail.com>
2025-01-11 17:41:28 +0900

    [Gold III] Title: 과제, Time: 40 ms, Memory: 35508 KB -BaekjoonHub

--- 백준/Gold/13904. 과제/과제.py
+++ 백준/Gold/13904. 과제/과제.py
@@ -4,19 +4,14 @@ input = sys.stdin.readline
 
 def main():
     n = int(input())
-    subject = [tuple(map(int, input().split())) for _ in range(n)]
-    sub = dict()
-    for d, w in subject:
-        if d in sub:
-            sub[d].append(-w)
-        else:
-            sub[d] = [0, -w]
+    subject = sorted([tuple(map(int, input().split())) for _ in range(n)], reverse=True)
     answer = 0
     heap = []
-    for d in range(max(sub.keys()), 0, -1):
-        if d in sub:
-            for x in sub[d]:
-                heappush(heap, x)
+    idx = 0
+    for d in range(subject[0][0], 0, -1):
+        while idx < n and subject[idx][0] == d:
+            heappush(heap, -subject[idx][1])
+            idx += 1
         if heap:
             answer -= heappop(heap)
     print(answer)
