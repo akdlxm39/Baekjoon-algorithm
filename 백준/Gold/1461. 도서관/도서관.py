3d3d2383c40c0caff11884b0a446d185ae28f974
Baek Jiung <zxcwldnd@gmail.com>
2025-01-11 17:18:06 +0900

    [Gold IV] Title: 도서관, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1461. 도서관/도서관.py
+++ 백준/Gold/1461. 도서관/도서관.py
@@ -6,12 +6,7 @@ def main():
     book = list(map(int, input().split()))
     left = sorted([-x for x in book if x < 0], reverse=True)
     right = sorted([x for x in book if x > 0], reverse=True)
-    answer = 0
-    for x in left[::m]:
-        answer += x * 2
-    for x in right[::m]:
-        answer += x * 2
-    answer -= max(left + right)
+    answer = sum(left[::m]) *2 + sum(right[::m]) *2 - max(left + right)
     print(answer)
 
 if __name__ == "__main__":
