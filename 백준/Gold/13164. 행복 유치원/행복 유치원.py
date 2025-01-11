29e0b7ed4837b9cdd396dcdc97fb49544216b39a
Baek Jiung <zxcwldnd@gmail.com>
2025-01-11 16:31:12 +0900

    [Gold V] Title: 행복 유치원, Time: 216 ms, Memory: 68700 KB -BaekjoonHub

---
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.py
@@ -0,0 +1,13 @@
+import sys
+from heapq import heapify, heappush, heappop
+input = sys.stdin.readline
+
+def main():
+    n, k = map(int, input().split())
+    price = list(map(int, input().split()))
+    diff = sorted([price[i] - price[i-1] for i in range(1, len(price))])
+    print(sum(diff[:n-k]))
+    
+        
+if __name__ == "__main__":
+    main()
