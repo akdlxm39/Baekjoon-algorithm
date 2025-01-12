2f12ce2771087763d04b56ee813b0838ce07ab3c
Baek Jiung <zxcwldnd@gmail.com>
2025-01-12 15:37:00 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 136 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.py
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.py
@@ -6,9 +6,8 @@ def main():
     for _ in range(T):
         n = int(input())
         l = sorted(list(map(int, input().split())))
-        ans = l[::2] + l[1::2][::-1] + [l[0]]
-        max_gap = max(abs(a-b) for a, b in zip(ans, ans[1:]))
-        print(max_gap)
+        level = max(abs(a-b) for a, b in zip(l, l[2:]))
+        print(level)
 
 if __name__ == "__main__":
     main()
