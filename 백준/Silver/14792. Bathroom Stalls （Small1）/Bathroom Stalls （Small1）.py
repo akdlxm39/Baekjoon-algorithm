bcee3d7b437932380d2a8025d3897495efe576b5
Baek Jiung <zxcwldnd@gmail.com>
2025-01-14 02:12:05 +0900

    [Silver V] Title: Bathroom Stalls (Small1), Time: 32 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Silver/14792. Bathroom Stalls （Small1）/Bathroom Stalls （Small1）.py
+++ 백준/Silver/14792. Bathroom Stalls （Small1）/Bathroom Stalls （Small1）.py
@@ -1,33 +1,14 @@
 import sys
-from collections import deque
+from math import log
 input = sys.stdin.readline
 
 def main():
     T = int(input())
     for i in range(1, T + 1):
         n, k = map(int, input().split())
-        dict = {n:1}
-        q = deque([n])
-        while k > 0 and q:
-            cur = q.popleft()
-            cnt = dict[cur]
-            max_gap, min_gap = cur // 2, (cur - 1) // 2
-            k -= cnt
-            if max_gap == 0:
-                continue
-            if max_gap in q:
-                dict[max_gap] += cnt
-            else:
-                dict[max_gap] = cnt
-                q.append(max_gap)
-            if min_gap == 0:
-                continue
-            if min_gap in q:
-                dict[min_gap] += cnt
-            else:
-                dict[min_gap] = cnt
-                q.append(min_gap)
-        print(f"Case #{i}:", max_gap, min_gap)
+        log_k = int(log(k, 2)) + 1
+        ans = (n-k)/(2**log_k)
+        print(f"Case #{i}: {int(ans+0.5)} {int(ans)}")
 
 if __name__ == "__main__":
     main()
