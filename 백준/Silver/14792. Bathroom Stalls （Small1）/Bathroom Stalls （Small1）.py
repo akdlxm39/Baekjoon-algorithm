fa360945d05480a50a06010fa490636e282a52bc
Baek Jiung <zxcwldnd@gmail.com>
2025-01-14 01:38:55 +0900

    [Silver V] Title: Bathroom Stalls (Small1), Time: 60 ms, Memory: 38092 KB -BaekjoonHub

--- 백준/Silver/14792. Bathroom Stalls （Small1）/Bathroom Stalls （Small1）.py
+++ 백준/Silver/14792. Bathroom Stalls （Small1）/Bathroom Stalls （Small1）.py
@@ -1,23 +1,33 @@
 import sys
-from collections import deque
 from heapq import heapify, heappush, heappop
+from collections import deque
 input = sys.stdin.readline
 
 def main():
-    gap = lambda x: ((x - 1) // 2 + (x - 1) % 2, (x - 1) // 2)
+    gap = lambda x: (x // 2, (x - 1) // 2)
     T = int(input())
     for i in range(1, T + 1):
         n, k = map(int, input().split())
-        heap = [-n]
-        max_gap = min_gap = 0
-        for _ in range(k):
-            cur = -heappop(heap)
+        dict = {n:1}
+        q = deque([n])
+        while q:
+            cur = q.popleft()
+            cnt = dict[cur]
             max_gap, min_gap = gap(cur)
-            if max_gap != 0:
-                heappush(heap, -max_gap)
-                if min_gap != 0:
-                    heappush(heap, -min_gap)
-        print(f"Case #{i}:", max_gap, min_gap)
+            if k <= cnt:
+                print(f"Case #{i}:", max_gap, min_gap)
+                break
+            k -= cnt
+            if max_gap in q:
+                dict[max_gap] += cnt
+            else:
+                dict[max_gap] = cnt
+                q.append(max_gap)
+            if min_gap in q:
+                dict[min_gap] += cnt
+            else:
+                dict[min_gap] = cnt
+                q.append(min_gap)
 
 if __name__ == "__main__":
     main()
