f455fc0e62c76eca0073f7488ff26fcc21340057
Baek Jiung <zxcwldnd@gmail.com>
2025-01-14 00:01:32 +0900

    [Gold V] Title: A와 B, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.py
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -4,18 +4,18 @@ input = sys.stdin.readline
 def main():
     s = list(input().rstrip())
     t = list(input().rstrip())
-    dir = 1
     while t:
         if s == t:
             print(1)
+            break
+        elif len(s) == len(t):
+            print(0)
             break
         if t[-1] == 'A':
             t.pop()
         else:
             t.pop()
             t.reverse()
-    else:
-        print(0)
 
 if __name__ == "__main__":
     main()
