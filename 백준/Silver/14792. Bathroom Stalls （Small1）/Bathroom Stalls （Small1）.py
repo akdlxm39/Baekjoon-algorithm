87003d50e19b56e0ff38f780937a72deb7274a8b
Baek Jiung <zxcwldnd@gmail.com>
2025-01-14 02:33:02 +0900

    [Silver V] Title: Bathroom Stalls (Small1), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14792. Bathroom Stalls （Small1）/Bathroom Stalls （Small1）.py
+++ 백준/Silver/14792. Bathroom Stalls （Small1）/Bathroom Stalls （Small1）.py
@@ -5,7 +5,7 @@ def main():
     T = int(input())
     for i in range(1, T + 1):
         n, k = map(int, input().split())
-        ans = (n - k) / (2 ** (len(bin(k))-2))
+        ans = (n - k) / (1 << (len(bin(k))-2))
         print(f"Case #{i}: {int(ans+0.5)} {int(ans)}")
 
 if __name__ == "__main__":
