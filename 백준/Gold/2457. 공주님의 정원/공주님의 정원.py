4d8010b7a0623ab8dce23a640bda57ace0953ed5
Baek Jiung <zxcwldnd@gmail.com>
2025-01-16 18:35:30 +0900

    [Gold III] Title: 공주님의 정원, Time: 204 ms, Memory: 43460 KB -BaekjoonHub

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.py
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.py
@@ -7,28 +7,29 @@ def main():
     n = int(input())
     month = [0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
     flowers = []
-    heap = []
+    new = 0
     idx = 0
     for _ in range(n):
         m1, d1, m2, d2 = map(int, input().split())
-        flowers.append((month[m1] + d1, month[m2] + d2))
-        if flowers[-1][0] <= 60:
-            heap.append(-flowers[-1][1])
-            idx += 1
+        if month[m1] + d1 <= 60:
+            new = max(new, month[m2] + d2)
+        else:
+            flowers.append((month[m1] + d1, month[m2] + d2))
     flowers.sort()
-    heapify(heap)
 
     cnt = 0
-    while heap:
+    while True:
         cnt += 1
-        cur = -heappop(heap)
+        cur = new
         if cur >= 335:
             print(cnt)
             break
-        while idx < n and flowers[idx][0] <= cur:
-            heappush(heap, -flowers[idx][1])
+        while idx < len(flowers) and flowers[idx][0] <= cur:
+            new = max(new, flowers[idx][1])
             idx += 1
-    else:
-        print(0)
+        if cur == new:
+            print(0)
+            break
+
 if __name__ == "__main__":
     main()
