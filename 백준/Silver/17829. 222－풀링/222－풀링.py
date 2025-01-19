fbfd8ac93b7b03bcb5ee32a24f0a53ef52da2c18
Baek Jiung <zxcwldnd@gmail.com>
2025-01-19 13:18:21 +0900

    [Silver II] Title: 222-풀링, Time: 396 ms, Memory: 73960 KB -BaekjoonHub

--- 백준/Silver/17829. 222－풀링/222－풀링.py
+++ 백준/Silver/17829. 222－풀링/222－풀링.py
@@ -1,17 +1,16 @@
 import sys
 input = sys.stdin.readline
 
-def solve(matrix, size, x, y):
-    if size == 1:
-        return matrix[x][y]
-    new_size = size//2
-    l = [solve(matrix, new_size, x, y), solve(matrix, new_size, x, y+new_size), solve(matrix, new_size, x+new_size, y), solve(matrix, new_size, x+new_size, y+new_size)]
-    return sorted(l)[2]
-
 def main():
     n = int(input())
     matrix = [list(map(int, input().split())) for _ in range(n)]
-    print(solve(matrix, n, 0, 0))
+    size = 1
+    while size < n:
+        for x in range(0, n, size*2):
+            for y in range(0, n, size*2):
+                matrix[x][y] = sorted([matrix[x][y], matrix[x][y + size], matrix[x + size][y], matrix[x + size][y + size]])[2]
+        size *= 2
+    print(matrix[0][0])
 
 if __name__ == "__main__":
     main()
