cec985d8c34a3b478b85a617d5d4bd524621f893
Baek Jiung <zxcwldnd@gmail.com>
2025-01-22 22:45:23 +0900

    [Gold III] Title: 트리 복구, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/6597. 트리 복구/트리 복구.py
+++ 백준/Gold/6597. 트리 복구/트리 복구.py
@@ -1,27 +1,13 @@
 import sys
 input = sys.stdin.readline
 
-class Node():
-    def __init__(self, value):
-        self.value : int = value
-        self.left : Node = None
-        self.right : Node = None
-
-def tree_recovery(preorder, inorder):
-    if not preorder:
-        return None
-    node = Node(preorder[0])
+def postorder(preorder, inorder):
+    if len(preorder) <= 1:
+        return preorder
     idx = inorder.index(preorder[0])
-    node.left = tree_recovery(preorder[1:idx+1], inorder[:idx])
-    node.right = tree_recovery(preorder[idx+1:], inorder[idx+1:])
-    return node
-
-def postordering(node, postorder):
-    if not node:
-        return
-    postordering(node.left, postorder)
-    postordering(node.right, postorder)
-    postorder.append(node.value)
+    return (postorder(preorder[1:idx + 1], inorder[:idx]) +
+            postorder(preorder[idx + 1:], inorder[idx + 1:]) +
+            inorder[idx])
 
 def main():
     while True:
@@ -29,10 +15,8 @@ def main():
             preorder, inorder = input().split()
         except:
             break
-        tree = tree_recovery(preorder, inorder)
-        ans = []
-        postordering(tree, ans)
-        print(''.join(ans))
+        postorder(preorder, inorder)
+        print(postorder(preorder, inorder))
 
 #preorder = cur, left, right
 #inorder = left, cur, right
