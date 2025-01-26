76b21e611f2d825eb8eb7e2bed74fb3c8098a1cb
Baek Jiung <zxcwldnd@gmail.com>
2025-01-26 14:46:44 +0900

    [Gold V] Title: 농장 관리, Time: 68 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Gold/1245. 농장 관리/농장 관리.py
+++ 백준/Gold/1245. 농장 관리/농장 관리.py
@@ -3,11 +3,10 @@ from collections import deque
 from itertools import product
 input = sys.stdin.readline
 
-def solve(n, m, farm, visited, queue, l):
+def solve(n, m, farm, visited, queue):
     flag = 1
     while queue:
         x, y = queue.popleft()
-        l.append((x, y))
         visited[x][y] = True
         for dx, dy in product([-1, 0, 1], repeat=2):
             nx, ny = x + dx, y + dy
@@ -25,14 +24,12 @@ def main():
     cnt = 0
     visited = [[False] * m for _ in range(n)]
     queue = deque()
-
     for i in range(n):
         for j in range(m):
             if visited[i][j]:
                 continue
-            l = []
             queue.append((i, j))
-            cnt += solve(n, m, farm, visited, queue, l)
+            cnt += solve(n, m, farm, visited, queue)
     print(cnt)
 
 if __name__ == "__main__":
