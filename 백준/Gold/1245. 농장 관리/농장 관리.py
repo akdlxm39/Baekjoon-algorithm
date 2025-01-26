23813ec60b0686f356269a1d975aac6815b3379c
Baek Jiung <zxcwldnd@gmail.com>
2025-01-26 14:44:28 +0900

    [Gold V] Title: 농장 관리, Time: 68 ms, Memory: 34992 KB -BaekjoonHub

---
+++ 백준/Gold/1245. 농장 관리/농장 관리.py
@@ -0,0 +1,39 @@
+import sys
+from collections import deque
+from itertools import product
+input = sys.stdin.readline
+
+def solve(n, m, farm, visited, queue, l):
+    flag = 1
+    while queue:
+        x, y = queue.popleft()
+        l.append((x, y))
+        visited[x][y] = True
+        for dx, dy in product([-1, 0, 1], repeat=2):
+            nx, ny = x + dx, y + dy
+            if (dx == 0 and dy == 0) or not (0 <= nx < n and 0 <= ny < m):
+                continue
+            if farm[nx][ny] == farm[x][y] and not visited[nx][ny]:
+                queue.append((nx, ny))
+            elif farm[nx][ny] > farm[x][y]:
+                flag = 0
+    return flag
+
+def main():
+    n, m = map(int, input().split())
+    farm = [list(map(int, input().split())) for _ in range(n)]
+    cnt = 0
+    visited = [[False] * m for _ in range(n)]
+    queue = deque()
+
+    for i in range(n):
+        for j in range(m):
+            if visited[i][j]:
+                continue
+            l = []
+            queue.append((i, j))
+            cnt += solve(n, m, farm, visited, queue, l)
+    print(cnt)
+
+if __name__ == "__main__":
+    main()
