e37297b936dbdeb7d3c901c40aa17a9fd2aa9709
Baek Jiung <zxcwldnd@gmail.com>
2025-01-27 18:08:36 +0900

    [Gold III] Title: 역사, Time: 448 ms, Memory: 39320 KB -BaekjoonHub

--- 백준/Gold/1613. 역사/역사.py
+++ 백준/Gold/1613. 역사/역사.py
@@ -1,22 +1,28 @@
 import sys
 input = sys.stdin.readline
 
+def solve(events, cur, visited):
+    visited[cur] = True
+    l = list(events[cur])
+    for nxt in l:
+        if not visited[nxt]:
+            solve(events, nxt, visited)
+        events[cur] |= events[nxt]
+
 def main():
     n, k = map(int, input().split())
-    events = [[0]*(n+1) for _ in range(n+1)]
+    events = [set() for _ in range(n+1)]
     for _ in range(k):
         x, y = map(int, input().split())
-        events[x][y] = 1
-    for k in range(1, n+1):
-        for x in range(1, n+1):
-            for y in range(1, n+1):
-                if events[x][k] and events[k][y]:
-                    events[x][y] = 1
+        events[x].add(y)
+    visited = [False] * (n + 1)
+    for i in range(1, n + 1):
+        solve(events, i, visited)
     for _ in range(int(input())):
         x, y = map(int, input().split())
-        if events[x][y]:
+        if y in events[x]:
             print(-1)
-        elif events[y][x]:
+        elif x in events[y]:
             print(1)
         else:
             print(0)
