a3457c4a90a75133be8946f3407ad838e2b947a8
Baek Jiung <zxcwldnd@gmail.com>
2025-01-27 15:37:10 +0900

    [Gold IV] Title: 미친 로봇, Time: 1036 ms, Memory: 37200 KB -BaekjoonHub

--- 백준/Gold/1405. 미친 로봇/미친 로봇.py
+++ 백준/Gold/1405. 미친 로봇/미친 로봇.py
@@ -3,26 +3,26 @@ from functools import reduce
 from math import gcd
 input = sys.stdin.readline
 
-def solve(n, visited, ratio, x=0, y=0):
+def solve(n, visited, ratio, x=14, y=14):
     if n == 0:
         return 1
     ans = 0
     dir = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-    visited.add((x, y))
+    visited[x][y] = True
     for i in range(4):
         dx, dy = x+dir[i][0], y + dir[i][1]
-        if (dx, dy) in visited:
+        if visited[dx][dy]:
             continue
         ans += ratio[i] * solve(n-1, visited, ratio, dx, dy)
-    visited.remove((x, y))
+    visited[x][y] = False
     return ans
 
 def main():
     n, *ratio = map(int, input().split())
     x = reduce(gcd, ratio)
     ratio = list(map(lambda a: a//x, ratio))
-    visited = set()
-    print(solve(n, visited, ratio, x)/(sum(ratio))**n)
+    visited = [[False]*29 for _ in range(29)]
+    print(solve(n, visited, ratio)/(sum(ratio))**n)
 
 if __name__ == "__main__":
     main()
