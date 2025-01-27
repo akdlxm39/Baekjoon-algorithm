deda768611189900bdba6aaf734fc7fb492fece3
Baek Jiung <zxcwldnd@gmail.com>
2025-01-27 17:30:20 +0900

    [Gold III] Title: 역사, Time: 2568 ms, Memory: 32700 KB -BaekjoonHub

--- 백준/Gold/1613. 역사/역사.py
+++ 백준/Gold/1613. 역사/역사.py
@@ -3,21 +3,23 @@ input = sys.stdin.readline
 
 def main():
     n, k = map(int, input().split())
-    next_event = [[0]*(n+1) for _ in range(n+1)]
+    events = [[0]*(n+1) for _ in range(n+1)]
     for _ in range(k):
         x, y = map(int, input().split())
-        next_event[x][y] = -1
-        next_event[y][x] = 1
+        events[x][y] = 1
     for k in range(1, n+1):
         for x in range(1, n+1):
             for y in range(1, n+1):
-                if next_event[x][k] == -1 and next_event[k][y] == -1:
-                    next_event[x][y] = -1
-                elif next_event[x][k] == 1 and next_event[k][y] == 1:
-                    next_event[x][y] = 1
+                if events[x][k] and events[k][y]:
+                    events[x][y] = 1
     for _ in range(int(input())):
         x, y = map(int, input().split())
-        print(next_event[x][y])
+        if events[x][y]:
+            print(-1)
+        elif events[y][x]:
+            print(1)
+        else:
+            print(0)
 
 if __name__ == "__main__":
     main()
