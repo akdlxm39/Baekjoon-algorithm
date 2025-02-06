0f5d74dd54ca77fd976dbafcc34b63c37ea9d4aa
Baek Jiung <zxcwldnd@gmail.com>
2025-02-06 16:49:25 +0900

    [Gold IV] Title: 네트워크 연결, Time: 192 ms, Memory: 51888 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -5,16 +5,16 @@ input = sys.stdin.readline
 def main():
     n, m = int(input()), int(input())
     costs = sorted((tuple(map(int, input().split())) for _ in range(m)), key = lambda x : x[2])
-    link1 = [x for x in range(n+1)]
-    link2 = dict([(x, [x]) for x in range(1, n+1)])
+    root = [x for x in range(n+1)]
+    link = dict([(x, {x}) for x in range(1, n+1)])
     ans = 0
     for a, b, cost in costs:
-        if link1[a] == link1[b]:
+        if root[a] == root[b]:
             continue
         ans += cost
-        for x in link2[link1[b]]:
-            link2[link1[a]].append(x)
-            link1[x] = link1[a]
+        link[root[a]] |= (link[root[b]])
+        for x in link[root[b]]:
+            root[x] = root[a]
     print(ans)
 
 if __name__ == "__main__":
