57d7cdd7499b4e7393cb0457d1a5e332a5139934
Baek Jiung <zxcwldnd@gmail.com>
2025-02-10 15:09:51 +0900

    [Gold III] Title: 택배, Time: 608 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.py
+++ 백준/Gold/1719. 택배/택배.py
@@ -1,5 +1,4 @@
 import sys
-from heapq import heapify, heappush, heappop
 input = sys.stdin.readline
 INF = int(1e9)
 
@@ -7,26 +6,20 @@ def main():
     n, m = map(int, input().split())
     costs = [[INF if i!=j else 0 for j in range(n+1)] for i in range(n+1)]
     ans = [['-']*(n+1) for _ in range(n+1)]
-    heap = []
     for _ in range(m):
         u, v, cost = map(int, input().split())
         costs[u][v] = costs[v][u] = cost
-        heappush(heap, (cost, u, v, 0))
-        heappush(heap, (cost, v, u, 0))
-    x = n*(n-1)
-    while x and heap:
-        cur_cost, s, e, nxt = heappop(heap)
-        if ans[s][e] != '-':
-            continue
-        ans[s][e] = nxt if nxt else e
+        ans[u][v] = v
+        ans[v][u] = u
+    for k in range(1, n+1):
         for i in range(1, n+1):
-            if ans[i][e] != '-' or i == e or costs[i][s]==INF:
-                continue
-            heappush(heap, (cur_cost + costs[i][s], i, e, s))
-        x-=1
-    for line in ans[1:]:
-        print(' '.join(map(str, line[1:])))
-
+            for j in range(1, n+1):
+                nxt_cost = costs[i][k] + costs[k][j]
+                if nxt_cost < costs[i][j]:
+                    costs[i][j] = nxt_cost
+                    ans[i][j] = ans[i][k]
+    for x in ans[1:]:
+        print(' '.join(map(str, x[1:])))
 
 if __name__ == "__main__":
     main()
