b018c41576f5d181644e740eec6f18c0cbab009f
Baek Jiung <zxcwldnd@gmail.com>
2025-02-23 01:07:21 +0900

    [Gold V] Title: 0 만들기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.py
+++ 백준/Gold/7490. 0 만들기/0 만들기.py
@@ -1,20 +1,20 @@
 import sys
 input = sys.stdin.readline
 
-def dfs(n, i, string, value, last):
+def dfs(n, i, value, last, string):
     if n < i:
         if value == 0:
             print(string)
         return
     now = 10*last + (i if last>0 else -i)
-    dfs(n, i+1, f"{string} {i}", value - last + now, now)
-    dfs(n, i + 1, f"{string}+{i}", value + i, i)
-    dfs(n, i + 1, f"{string}-{i}", value - i, -i)
+    dfs(n, i+1, value - last + now, now, f"{string} {i}")
+    dfs(n, i + 1, value + i, i, f"{string}+{i}")
+    dfs(n, i + 1, value - i, -i, f"{string}-{i}")
 
 def main():
     for _ in range(int(input())):
         n = int(input())
-        dfs(n, 2, '1', 1, 1)
+        dfs(n, 2, 1, 1, '1')
         print()
 
 if __name__ == "__main__":
