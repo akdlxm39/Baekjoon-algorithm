6d701fa99df835b2842183a695a65ab8cc7c5fab
Baek Jiung <zxcwldnd@gmail.com>
2025-02-23 01:13:45 +0900

    [Gold V] Title: 0 만들기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.py
+++ 백준/Gold/7490. 0 만들기/0 만들기.py
@@ -7,14 +7,13 @@ def dfs(n, i, value, last, string):
             print(string)
         return
     now = 10*last + (i if last>0 else -i)
-    dfs(n, i+1, value - last + now, now, f"{string} {i}")
-    dfs(n, i + 1, value + i, i, f"{string}+{i}")
-    dfs(n, i + 1, value - i, -i, f"{string}-{i}")
+    dfs(n, i+1, value-last+now, now, f"{string} {i}")
+    dfs(n, i+1, value+i, i, f"{string}+{i}")
+    dfs(n, i+1, value-i, -i, f"{string}-{i}")
 
 def main():
     for _ in range(int(input())):
-        n = int(input())
-        dfs(n, 2, 1, 1, '1')
+        dfs(int(input()), 2, 1, 1, '1')
         print()
 
 if __name__ == "__main__":
