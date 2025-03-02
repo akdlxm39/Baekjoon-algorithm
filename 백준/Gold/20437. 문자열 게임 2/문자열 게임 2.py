56f96a6b52169227d55721b8278b0ac7887c5e2c
Baek Jiung <zxcwldnd@gmail.com>
2025-03-02 14:43:14 +0900

    [Gold V] Title: 문자열 게임 2, Time: 96 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.py
+++ 백준/Gold/20437. 문자열 게임 2/문자열 게임 2.py
@@ -15,9 +15,9 @@ def main():
             for l in cp[97:]:
                 if len(l) < k:
                     continue
-                for x, y in zip(l, l[k-1:]):
-                    ans1 = min(ans1, y - x + 1)
-                    ans2 = max(ans2, y - x + 1)
+                width = [y-x+1 for x, y in zip(l, l[k-1:])]
+                ans1 = min(ans1, min(width))
+                ans2 = max(ans2, max(width))
         if ans1 == 10000 or ans2 == 0:
             print(-1)
         else:
