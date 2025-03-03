5c2abce915e76a3c6577affbc8276648aacac720
Baek Jiung <zxcwldnd@gmail.com>
2025-03-03 12:46:27 +0900

    [Gold III] Title: 디스크 트리, Time: 36 ms, Memory: 34876 KB -BaekjoonHub

--- 백준/Gold/7432. 디스크 트리/디스크 트리.py
+++ 백준/Gold/7432. 디스크 트리/디스크 트리.py
@@ -4,13 +4,13 @@ input = sys.stdin.readline
 def main():
     n = int(input())
     dirs = sorted(input().rstrip().split('\\') for _ in range(n))
-    ans = [' '*blank + dir for blank, dir in enumerate(dirs[0])]
+    ans = [' '*blank + file for blank, file in enumerate(dirs[0])]
     for i in range(1, len(dirs)):
         cnt = 0
         while cnt <len(dirs[i-1]) and cnt < len(dirs[i]) and dirs[i-1][cnt] == dirs[i][cnt]:
             cnt += 1
-        for blank, dir in enumerate(dirs[i][cnt:], cnt):
-            ans.append(' '*blank + dir)
+        for blank, file in enumerate(dirs[i][cnt:], cnt):
+            ans.append(' '*blank + file)
     print('\n'.join(ans))
 
 if __name__ == "__main__":
