89454eaaa3c87b17f8802bc3f77309a5538b96a9
Baek Jiung <zxcwldnd@gmail.com>
2025-03-07 16:48:12 +0900

    [Gold I] Title: 문자열 거리, Time: 180 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1230. 문자열 거리/문자열 거리.py
+++ 백준/Gold/1230. 문자열 거리/문자열 거리.py
@@ -4,24 +4,26 @@ input = sys.stdin.readline
 def main():
     o = input().rstrip()
     n = input().rstrip()
-    dp = [(0, True)] + [(0, False) for _ in range(len(n))]
-    for i, x in enumerate(o, 1):
-        tmp = [(0, False)]
+    dp = [1] + [0]*len(n)
+    for x in o:
+        tmp = [0]
         mindp = 1000
-        for j, y in enumerate(n, 1):
-            if dp[j-1][1]:
-                mindp = min(mindp, dp[j-1][0])
+        for i, y in enumerate(n, 1):
+            if dp[i-1]:
+                mindp = min(mindp, dp[i-1])
             if x == y:
-                if dp[j-1][1]:
-                    tmp.append((dp[j-1][0], True))
+                if dp[i-1]:
+                    tmp.append(dp[i-1])
                 elif mindp != 1000:
-                    tmp.append((mindp+1, True))
+                    tmp.append(mindp+1)
                 else:
-                    tmp.append((0, False))
+                    tmp.append(0)
             else:
-                tmp.append((0, False))
+                tmp.append(0)
         dp = tmp
-    ans = [x + (i!=len(n)) for i, (x, b) in enumerate(dp) if b]
+    ans = [x for x in dp[:-1] if x]
+    if dp[-1]:
+        ans.append(dp[-1] - 1)
     print(min(ans) if ans else -1)
 
 if __name__ == "__main__":
