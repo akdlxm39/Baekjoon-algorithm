c4d42261bb87d606ff60e70bc141800cefea14e3
Baek Jiung <zxcwldnd@gmail.com>
2025-03-07 15:51:05 +0900

    [Gold I] Title: 문자열 거리, Time: 212 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1230. 문자열 거리/문자열 거리.py
+++ 백준/Gold/1230. 문자열 거리/문자열 거리.py
@@ -4,27 +4,25 @@ input = sys.stdin.readline
 def main():
     o = input().rstrip()
     n = input().rstrip()
-    dp = [[[0, False] for _ in range(len(n)+1)] for _ in range(len(o)+1)]
-    dp[0][0][1] = True
+    dp = [(0, True)] + [(0, False) for _ in range(len(n))]
     for i, x in enumerate(o, 1):
+        tmp = [(0, False)]
+        mindp = 1000
         for j, y in enumerate(n, 1):
+            if dp[j-1][1]:
+                mindp = min(mindp, dp[j-1][0])
             if x == y:
-                if dp[i-1][j-1][1]:
-                    dp[i][j][0] = dp[i-1][j-1][0]
-                    dp[i][j][1] = True
+                if dp[j-1][1]:
+                    tmp.append((dp[j-1][0], True))
+                elif mindp != 1000:
+                    tmp.append((mindp+1, True))
                 else:
-                    tmp = [x[0] for x in dp[i-1][:j-1] if x[1]]
-                    if tmp:
-                        dp[i][j][0] = min(tmp)+1
-                        dp[i][j][1] = True
-    ans = len(n)
-    for i in range(1, len(n)+2):
-        if i == len(n)+1:
-            if dp[-1][-1][1]:
-                ans = min(ans, dp[-1][-1][0])
-            break
-        if dp[-1][i][1]:
-            ans = min(ans, dp[-1][i][0]+1)
-    print(ans if ans!=len(n) else -1)
+                    tmp.append((0, False))
+            else:
+                tmp.append((0, False))
+        dp = tmp
+    ans = [x + (i!=len(n)) for i, (x, b) in enumerate(dp) if b]
+    print(min(ans) if ans else -1)
+
 if __name__ == "__main__":
     main()
