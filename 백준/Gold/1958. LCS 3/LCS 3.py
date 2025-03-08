8d41a571461bc0999927e56284925a0b4aa37763
Baek Jiung <zxcwldnd@gmail.com>
2025-03-08 13:55:22 +0900

    [Gold IV] Title: LCS 3, Time: 228 ms, Memory: 40560 KB -BaekjoonHub

--- 백준/Gold/1958. LCS 3/LCS 3.py
+++ 백준/Gold/1958. LCS 3/LCS 3.py
@@ -4,10 +4,10 @@ input = sys.stdin.readline
 def main():
     s1, s2, s3 = input().rstrip(), input().rstrip(), input().rstrip()
     dp = [[[0]*(len(s3)+1) for _ in range(len(s2)+1)] for _ in range(len(s1)+1)]
-    for i in range(1, len(s1)+1):
-        for j in range(1, len(s2)+1):
-            for k in range(1, len(s3)+1):
-                if s1[i-1] == s2[j-1] == s3[k-1]:
+    for i, c1 in enumerate(s1, 1):
+        for j, c2 in enumerate(s2, 1):
+            for k, c3 in enumerate(s3, 1):
+                if c1 == c2 == c3:
                     dp[i][j][k] = dp[i-1][j-1][k-1] + 1
                 else:
                     dp[i][j][k] = max(dp[i-1][j][k], dp[i][j-1][k], dp[i][j][k-1])
