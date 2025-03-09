17d28b94b2642eb252602c44b074b6304e3d7657
Baek Jiung <zxcwldnd@gmail.com>
2025-03-09 21:32:05 +0900

    [Gold IV] Title: 괄호 제거, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2800. 괄호 제거/괄호 제거.py
+++ 백준/Gold/2800. 괄호 제거/괄호 제거.py
@@ -1,34 +1,20 @@
 import sys
 input = sys.stdin.readline
 
-def get_pairs(pairs, i):
-    l, r = pairs[i]
-    res = [l]
-    i += 1
-    while i < len(pairs) and pairs[i][1] < r:
-        tmp, j = get_pairs(pairs, i)
-        res.append(tmp)
-        i = j
-    res.append(r)
-    return res, i
-
-def dfs(s, pairs):
-    if not pairs:
-        return ['']
-    res = ['']
-    prev_r = pairs[0]+1
-    for pair in pairs[1:-1]:
-        cur_l, cur_r = pair[0], pair[-1]
-        tmp_s = s[prev_r:cur_l]
-        exps = dfs(s, pair)
-        res = [x+tmp_s+exp for x in res for exp in exps]
-        prev_r = cur_r+1
-    tmp_s = s[prev_r:pairs[-1]] if s[prev_r:pairs[-1]] else ''
-    res = ['('+x+tmp_s+')' for x in res] + [x+tmp_s for x in res]
-    return res
+def dfs(s, pairs, i, exps):
+    if i == len(pairs):
+        exps.append(''.join(s))
+        return
+    dfs(s, pairs, i + 1, exps)
+    s[pairs[i][0]] = ''
+    s[pairs[i][1]] = ''
+    dfs(s, pairs, i + 1, exps)
+    s[pairs[i][0]] = '('
+    s[pairs[i][1]] = ')'
+    return
 
 def main():
-    s = input().rstrip()
+    s = list(input().rstrip())
     pairs = []
     stack = []
     for i, c in enumerate(s):
@@ -36,11 +22,10 @@ def main():
             stack.append(i)
         elif c == ')':
             pairs.append((stack.pop(), i))
-    pairs, _ = get_pairs(sorted(pairs), 0)
-    exps = dfs(s, pairs)
-    ans = [s[:pairs[0]] + x + s[pairs[-1]+1:] for x in exps]
-    ans = sorted(set(ans))
-    print(*ans[1:],sep='\n')
+    exps = []
+    dfs(s, pairs, 0, exps)
+    ans = sorted(set(exps))
+    print(*ans[1:], sep='\n')
 
 if __name__ == "__main__":
     main()
