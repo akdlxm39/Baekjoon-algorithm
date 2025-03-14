bb1fec10f69c82fc175ef48eb2d49a263b81b252
Baek Jiung <zxcwldnd@gmail.com>
2025-03-14 15:58:16 +0900

    [Gold IV] Title: 사이클 게임, Time: 436 ms, Memory: 52700 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.py
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.py
@@ -1,29 +1,26 @@
 import sys
 input = sys.stdin.readline
 
-def get_root(roots,u):
+def find(roots, u):
     if roots[u] == u:
         return u
-    roots[u] = get_root(roots, roots[u])
+    roots[u] = find(roots, roots[u])
     return roots[u]
 
-def union_and_checkCycle(roots, u, v):
-    ur, vr = get_root(roots, u), get_root(roots, v)
-    if ur == vr:
-        return True
-    elif ur < vr:
+def union(roots, u, v):
+    ur, vr = find(roots, u), find(roots, v)
+    if ur < vr:
         roots[vr] = ur
     else:
         roots[ur] = vr
-    return False
+    return ur == vr
 
 def main():
     n, m = map(int, input().split())
     roots = list(range(n))
-    ans = 0
     for i in range(1, m+1):
         u, v = map(int, input().split())
-        if union_and_checkCycle(roots, u, v):
+        if union(roots, u, v):
             print(i)
             break
     else:
