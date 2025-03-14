90b2a6a737dfe5e9bc0998eafb02fa23fccf6785
Baek Jiung <zxcwldnd@gmail.com>
2025-03-14 17:29:28 +0900

    [Silver IV] Title: 짝수? 홀수?, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11815. 짝수？ 홀수？/짝수？ 홀수？.py
+++ 백준/Silver/11815. 짝수？ 홀수？/짝수？ 홀수？.py
@@ -1,12 +1,10 @@
 import sys
-from math import sqrt
 input = sys.stdin.readline
 
 def main():
     input()
     for x in map(int, input().split()):
-        y = int(sqrt(x))
-        print(int(y*y == x), end=' ')
+        print(int(x == (int(x**0.5)**2)), end=' ')
 
 if __name__ == "__main__":
     main()
