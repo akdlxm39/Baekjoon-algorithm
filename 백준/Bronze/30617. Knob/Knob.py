9507754bf4b42147975b2b5953f730138066522d
Baek Jiung <zxcwldnd@gmail.com>
2025-03-14 16:27:27 +0900

    [Bronze III] Title: Knob, Time: 156 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/30617. Knob/Knob.py
+++ 백준/Bronze/30617. Knob/Knob.py
@@ -3,12 +3,11 @@ input = sys.stdin.readline
 
 def main():
     t = int(input())
-    prev_l, prev_r = map(int, input().split())
-    ans = int(prev_l and prev_r and prev_l==prev_r)
+    l, r = map(int, input().split())
+    ans = int(l and l==r)
     for _ in range(t-1):
-        l, r = map(int, input().split())
+        (prev_l, prev_r), (l, r) = (l, r), map(int, input().split())
         ans += (l and l==prev_l) + (r and r==prev_r) + (l and l==r)
-        prev_l, prev_r = l, r
     print(ans)
 
 if __name__ == "__main__":
