464b162f3ee47a376bf23095d905fc1f1267e6c3
Baek Jiung <zxcwldnd@gmail.com>
2025-03-14 16:17:35 +0900

    [Bronze III] Title: Knob, Time: 156 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/30617. Knob/Knob.py
+++ 백준/Bronze/30617. Knob/Knob.py
@@ -7,12 +7,7 @@ def main():
     ans = int(prev_l and prev_r and prev_l==prev_r)
     for _ in range(t-1):
         l, r = map(int, input().split())
-        if l:
-            ans += l==prev_l
-        if r:
-            ans += r==prev_r
-        if l and r:
-            ans += l==r
+        ans += (l and l==prev_l) + (r and r==prev_r) + (l and l==r)
         prev_l, prev_r = l, r
     print(ans)
 
