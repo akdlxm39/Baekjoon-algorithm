8ba44fa0547fd5c61371e8e7c4f0d1a31b569eda
Baek Jiung <zxcwldnd@gmail.com>
2025-03-14 17:13:59 +0900

    [Silver V] Title: Alloys, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/20761. Alloys/Alloys.py
+++ 백준/Silver/20761. Alloys/Alloys.py
@@ -3,8 +3,7 @@ input = sys.stdin.readline
 
 def main():
     f = float(input())
-    f = f if f<=1 else 1
-    ans = (f*f)/4
+    ans = (min(1, f))**2/4
     print(ans)
 
 if __name__ == "__main__":
