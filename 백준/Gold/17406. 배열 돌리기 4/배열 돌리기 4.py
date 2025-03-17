f9fed20f747389a0b4ec6b43f7bde16dde799568
Baek Jiung <zxcwldnd@gmail.com>
2025-03-17 18:32:49 +0900

    [Gold IV] Title: 배열 돌리기 4, Time: 336 ms, Memory: 35016 KB -BaekjoonHub

---
+++ 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.py
@@ -0,0 +1,40 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+delta = [(0,1), (1,0), (0,-1), (-1,0)]
+
+def shuffle(k, matrix, opers, used, cnt):
+    if cnt == k:
+        return min(sum(line) for line in matrix)
+    ans = 5001
+    for i in range(k):
+        if used[i]:
+            continue
+        used[i] = True
+        r, c, s = opers[i]
+        tmp = shuffle(k, rotation(matrix, r-1, c-1, s), opers, used, cnt+1)
+        ans = min(ans, tmp)
+        used[i] = False
+    return ans
+
+def rotation(matrix, r, c, s):
+    new_matrix = [[i for i in m] for m in matrix]
+    for d in range(1, s+1):
+        x, y = r-d, c-d
+        for dx, dy in delta:
+            for _ in range(2*d):
+                x, y, px, py = x + dx, y + dy, x, y
+                new_matrix[x][y] = matrix[px][py]
+    return new_matrix
+    
+def main():
+    n, m, k = map(int, input().split())
+    matrix = [list(map(int, input().split())) for _ in range(n)]
+    opers = [list(map(int, input().split())) for _ in range(k)]
+    used = [False]*k
+    ans = shuffle(k, matrix, opers, used, 0)
+    print(ans)
+
+if __name__ == "__main__":
+    main()
