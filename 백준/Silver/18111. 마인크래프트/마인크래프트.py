8acee49d5703f35485017e2f406f476962169e34
Baek Jiung <zxcwldnd@gmail.com>
2025-03-21 19:22:21 +0900

    [Silver II] Title: 마인크래프트, Time: 84 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -2,33 +2,29 @@ import sys
 input = sys.stdin.readline
 
 def main():
-    n, m, b = map(int, input().split())
+    n, m, block = map(int, input().split())
     heights = dict()
     for _ in range(n):
         for x in map(int, input().split()):
             heights[x] = heights.get(x, 0) + 1
-    min_height = min(heights.keys())
-    max_height = max(heights.keys())
+    height = min(heights.keys())
     size = n*m
-    
     time = 0
-    block = b
     for cur, cnt in heights.items():
-        tmp = (cur - min_height) * cnt
+        tmp = (cur - height) * cnt
         time += 2 * tmp
         block += tmp
-    need = heights[min_height]
-    ans = (time, min_height)
-    for height in range(min_height+1, max_height+1):
-        if block >= size:
-            time += 3*need - 2*size
-            block -= size
-            if time <= ans[0]:
-                ans = (time, height)
-        else:
-            break
+    need = heights[height]
+    ans = (time, height)
+    height += 1
+    while block >= size:
+        time += 3*need - 2*size
+        block -= size
+        if time <= ans[0]:
+            ans = (time, height)
         if height in heights:
             need += heights[height]
+        height += 1
     print(*ans)
 
 if __name__ == "__main__":
