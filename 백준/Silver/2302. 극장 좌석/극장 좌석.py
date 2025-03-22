b28ee6de8abe2c723619265c6d42eb823c592179
Baek Jiung <zxcwldnd@gmail.com>
2025-03-22 11:30:04 +0900

    [Silver I] Title: 극장 좌석, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2302. 극장 좌석/극장 좌석.py
+++ 백준/Silver/2302. 극장 좌석/극장 좌석.py
@@ -2,17 +2,16 @@ import sys
 input = sys.stdin.readline
 
 def main():
-    N = int(input())
-    M = int(input())
-    vip = [0] + [int(input()) for _ in range(M)] + [N+1]
-    gap = [vip[i] - vip[i-1] - 1 for i in range(1, len(vip))]
+    n, m = int(input()), int(input())
+    vip = [0] + [int(input()) for _ in range(m)] + [n+1]
+    gap = [vip[i]-vip[i-1]-1 for i in range(1, m+2)]
     dp = [1, 1]
     for i in range(2, max(gap)+1):
         dp.append(dp[i-2] + dp[i-1])
-    answer = 1
-    for g in gap:
-        answer *= dp[g]
-    print(answer)
+    ans = 1
+    for x in gap:
+        ans *= dp[x]
+    print(ans)
 
 if __name__ == "__main__":
     main()
