b9ebd0484d0b9ce94621324d0ea4f3a0d485bde3
Baek Jiung <zxcwldnd@gmail.com>
2025-03-24 22:19:06 +0900

    [Gold V] Title: 배, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.py
+++ 백준/Gold/1092. 배/배.py
@@ -9,24 +9,27 @@ def main():
     if cranes[0] < boxes[0]:
         print(-1)
         return
-    while cranes and cranes[-1] < boxes[-1]:
-        cranes.pop()
-        n -= 1
-    ans = 0
-    while m:
-        i = 0
-        for j in range(n):
-            if m == 0:
-                break
-            elif cranes[j] < boxes[-1]:
-                n = j
-                break
-            while i < m and cranes[j] < boxes[i]:
-                i += 1
-            boxes.pop(i)
-            m -= 1
-        ans += 1
-    print(ans)
+    max_cnt = []
+    i = 0
+    for crane in cranes[1:]:
+        cnt = i
+        while i < m and crane < boxes[i]:
+            i += 1
+        max_cnt.append(i-cnt)
+        if i == m:
+            break
+    else:
+        max_cnt.append(m-i)
+    h = remain = 0
+    for w, cur_cnt in enumerate(max_cnt, 1):
+        cur = cur_cnt - remain
+        if cur > h:
+            cur -= h
+            h += (cur-1)//w+1
+            remain = (w-cur%w) if cur%w else 0
+        else:
+            remain = h-cur
+    print(h)
 
 if __name__ == "__main__":
     main()
