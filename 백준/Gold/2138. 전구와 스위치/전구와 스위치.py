8916bab66a330878b71b140c1267c92887a50432
Baek Jiung <zxcwldnd@gmail.com>
2025-03-24 23:32:16 +0900

    [Gold IV] Title: 전구와 스위치, Time: 64 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+def main():
+    n = int(input())
+    lamp1 = input().rstrip()
+    lamp2 = input().rstrip()
+    # state 0: !click and !need / 1: !click and need / 2: click and !need / 3: click and need
+    case1_flag, case2_flag = (False, False), (False, True)
+    case1_cnt = case2_cnt = 0
+    for i in range(n):
+        case1_cnt += case1_flag[1]
+        case1_flag = (case1_flag[1], (case1_flag[0]==case1_flag[1])!=(lamp1[i]==lamp2[i]))
+        case2_cnt += case2_flag[1]
+        case2_flag = (case2_flag[1], (case2_flag[0]==case2_flag[1])!=(lamp1[i]==lamp2[i]))
+    if case1_flag[1]:
+        case1_cnt = 100001
+    if case2_flag[1]:
+        case2_cnt = 100001
+    ans = min(case1_cnt, case2_cnt)
+    print(ans if ans != 100001 else -1)
+
+if __name__ == "__main__":
+    main()
