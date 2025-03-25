535114c1a57504b6057d819dafce4856955cdbb7
Baek Jiung <zxcwldnd@gmail.com>
2025-03-25 22:36:59 +0900

    [Gold II] Title: 보석 도둑, Time: 716 ms, Memory: 114048 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -8,17 +8,17 @@ def main():
     for _ in range(n):
         m, v = map(int, input().split())
         if m in jewels:
-            jewels[m].append(-v)
+            jewels[m].append(v)
         else:
-            jewels[m] = [-v]
+            jewels[m] = [v]
     jewels_m = sorted(jewels.keys())
     bags = sorted(int(input()) for _ in range(k))
     can_put = []
     ans = index = 0
     for bag in bags:
         while index < len(jewels_m) and jewels_m[index] <= bag:
-            for x in jewels[jewels_m[index]]:
-                heappush(can_put, x)
+            for v in jewels[jewels_m[index]]:
+                heappush(can_put, -v)
             index += 1
         if can_put:
             ans -= heappop(can_put)
