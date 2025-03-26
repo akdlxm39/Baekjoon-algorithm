afc4c3f75a55df000340dd73614fe4291f90cd21
Baek Jiung <zxcwldnd@gmail.com>
2025-03-27 00:44:28 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 4828 ms, Memory: 50004 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
@@ -3,28 +3,47 @@ from collections import deque
 input = sys.stdin.readline
 
 def bfs(n, m, k, maps):
-    visited = [[-1]*m for _ in range(n)]
-    queue = deque([(0, 0, 1, k)])
-    visited[0][0] = k
-    while queue:
-        x, y, dist, can_break = queue.popleft()
-        if visited[x][y] > can_break:
-            continue
-        for dx, dy in [(0,1),(1,0),(0,-1),(-1,0)]:
-            nx, ny = x+dx, y+dy
-            if not (0<=nx<n and 0<=ny<m) or can_break <= visited[nx][ny]:
-                continue
-            if nx == n-1 and ny == m-1:
-                return dist+1
-            nxt_can_break = can_break-(maps[nx][ny]=='1')
-            visited[nx][ny] = nxt_can_break
-            if nxt_can_break >= 0:
-                queue.append((nx, ny, dist+1, nxt_can_break))
-    return -1
+    size = n*m
+    visited = [-1]*size
+    queue = [0]
+    visited[0] = k
+    dist = 1
+    while queue and visited[-1] == -1:
+        nxt_queue = []
+        for x in queue:
+            nx = x+1
+            if nx % m:
+                nb = visited[x]-(maps[nx]=='1')
+                if nb > visited[nx]:
+                    visited[nx] = nb
+                    nxt_queue.append(nx)
+            nx = x-1
+            if x % m:
+                nb = visited[x]-(maps[nx]=='1')
+                if nb > visited[nx]:
+                    visited[nx] = nb
+                    nxt_queue.append(nx)
+            nx = x+m
+            if nx < size:
+                nb = visited[x]-(maps[nx]=='1')
+                if nb > visited[nx]:
+                    visited[nx] = nb
+                    nxt_queue.append(nx)
+            nx = x-m
+            if nx >= 0:
+                nb = visited[x]-(maps[nx]=='1')
+                if nb > visited[nx]:
+                    visited[nx] = nb
+                    nxt_queue.append(nx)
+        queue = nxt_queue
+        dist += 1
+    return dist if visited[-1] != -1 else -1
 
 def main():
     n, m, k = map(int, input().split())
-    maps = [input().rstrip() for _ in range(n)]
+    maps = ''
+    for _ in range(n):
+        maps += input().rstrip()
     print(1 if n==1 and m==1 else bfs(n, m, k, maps))
 
 if __name__ == "__main__":
