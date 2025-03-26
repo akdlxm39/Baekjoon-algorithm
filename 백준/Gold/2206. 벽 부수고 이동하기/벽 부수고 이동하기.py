2547cfd21150b48dea83c17698e5f1fe3c805189
Baek Jiung <zxcwldnd@gmail.com>
2025-03-26 15:25:39 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 2644 ms, Memory: 63912 KB -BaekjoonHub

---
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def bfs(n, m, maps):
+    visited = [[0]*m for _ in range(n)]
+    queue = deque([(0, 0, 1, True)])
+    visited[0][0] = 2
+    while queue:
+        x, y, dist, can_break = queue.popleft()
+        for dx, dy in [(0,1),(1,0),(0,-1),(-1,0)]:
+            nx, ny = x+dx, y+dy
+            if nx == n-1 and ny == m-1:
+                return dist+1
+            elif not (0<=nx<n and 0<=ny<m) or visited[nx][ny] == 2:
+                continue
+            if visited[nx][ny]<2 and maps[nx][ny]=='0' and can_break:
+                visited[nx][ny] = 2
+                queue.append((nx, ny, dist+1, can_break))
+            elif visited[nx][ny]==0 and not(maps[nx][ny]=='1' and not can_break):
+                visited[nx][ny] = 1
+                queue.append((nx, ny, dist+1, False))
+    return -1
+
+def main():
+    n, m = map(int, input().split())
+    maps = [input().rstrip() for _ in range(n)]
+    print(1 if n+m==2 else bfs(n, m, maps))
+
+if __name__ == "__main__":
+    main()
