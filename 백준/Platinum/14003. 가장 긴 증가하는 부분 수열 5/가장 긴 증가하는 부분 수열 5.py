9b332ae70547e56c51afe03ec148f0b7b1c308f1
Baek Jiung <zxcwldnd@gmail.com>
2025-03-27 19:27:45 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 864 ms, Memory: 191700 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.py
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.py
@@ -1,38 +1,29 @@
 import sys
+from bisect import bisect_left
 input = sys.stdin.readline
-
-def lower_bound(arr, target):
-    l, r = 0, len(arr)-1
-    while l <= r:
-        mid = (l+r)//2
-        if arr[mid] < target:
-            l = mid+1
-        else:
-            r = mid-1
-    return l
 
 def main():
     n = int(input())
-    num_list = list(map(int, input().split()))
+    num_list = input().split()
     stack = []
     dp = [0] * n
-    for i, num in enumerate(num_list):
+    for i in range(n):
+        num = int(num_list[i])
         if not stack or stack[-1] < num:
             stack.append(num)
             dp[i] = len(stack)
         else:
-            index = lower_bound(stack, num)
+            index = bisect_left(stack, num)
             stack[index] = num
             dp[i] = index+1
     cnt = len(stack)
-    ans = [0]*cnt
-    while cnt and n:
-        n -= 1
-        if dp[n] == cnt:
+    ans = [""]*cnt
+    for i in range(n-1,-1,-1):
+        if dp[i] == cnt:
             cnt -= 1
-            ans[cnt] = num_list[n]
+            ans[cnt] = num_list[i]
     print(len(ans))
-    print(*ans)
+    print(' '.join(ans))
 
 if __name__ == "__main__":
     main()
