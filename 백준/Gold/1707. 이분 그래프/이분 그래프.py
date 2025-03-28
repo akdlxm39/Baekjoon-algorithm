bded2ef10d8e78c9f1b2ada190481976e73679bc
Baek Jiung <zxcwldnd@gmail.com>
2025-03-28 15:29:46 +0900

    [Gold IV] Title: 이분 그래프, Time: 1108 ms, Memory: 54592 KB -BaekjoonHub

---
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -0,0 +1,41 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def bfs(adj_list, groups, x):
+    groups[x] = 1
+    queue = deque([(x, 1)])
+    while queue:
+        cur, group_num = queue.popleft()
+        for nxt in adj_list[cur]:
+            if groups[nxt] == group_num:
+                return False
+            if groups[nxt] == 0:
+                groups[nxt] = 3-group_num
+                queue.append((nxt, 3-group_num))
+    return True
+
+def main():
+    k = int(input())
+    for _ in range(k):
+        vertex, edge = map(int, input().split())
+        adj_list = dict()
+        for _ in range(edge):
+            u, v = map(int, input().split())
+            if u not in adj_list:
+                adj_list[u] = []
+            if v not in adj_list:
+                adj_list[v] = []
+            adj_list[u].append(v)
+            adj_list[v].append(u)
+        groups = [0]*(vertex+1)
+        for i in adj_list.keys():
+            if groups[i] == 0:
+                if not bfs(adj_list, groups, i):
+                    print("NO")
+                    break
+        else:
+            print("YES")
+
+if __name__ == "__main__":
+    main()
