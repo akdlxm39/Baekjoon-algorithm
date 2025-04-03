ad10ab4ece6dfb82bb804182cd276ad3670d2181
Baek Jiung <zxcwldnd@gmail.com>
2025-04-03 17:12:50 +0900

    [Gold I] Title: 이사, Time: 252 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/17371. 이사/이사.py
+++ 백준/Gold/17371. 이사/이사.py
@@ -3,14 +3,9 @@ input = sys.stdin.readline
 
 def main():
     n = int(input())
-    amenities = set(tuple(map(int, input().split())) for _ in range(n))
-    ans = (0, 0)
-    length_min = 1000000000000
-    for ax, ay in amenities:
-        length_max = max((bx-ax)**2+(by-ay)**2 for bx, by in amenities)
-        if length_max < length_min:
-            length_min = length_max
-            ans = (ax, ay)
+    amenities = [tuple(map(int, input().split())) for _ in range(n)]
+    min_cmp = lambda a:max((x-a[0])**2+(y-a[1])**2 for x, y in amenities)
+    ans = min(amenities, key=min_cmp)
     print(*ans)
 
 if __name__ == "__main__":
