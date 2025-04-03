d630fd4926544bf9ca8509c933a787508f1c231d
Baek Jiung <zxcwldnd@gmail.com>
2025-04-03 16:49:40 +0900

    [Gold I] Title: 이사, Time: 284 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/17371. 이사/이사.py
+++ 백준/Gold/17371. 이사/이사.py
@@ -3,21 +3,18 @@ input = sys.stdin.readline
 
 def main():
     n = int(input())
-    amenities = [tuple(map(int, input().split())) for _ in range(n)]
+    amenities = set(tuple(map(int, input().split())) for _ in range(n))
     length_min = 1000000000000
-    ans = amenities[0]
-    for i in range(n):
+    ans = amenities.pop()
+    amenities.add(ans)
+    for ax, ay in amenities:
         length_max = 0
-        tmp = amenities[i]
-        for j in range(n):
-            if i == j:
-                continue
-            length = (amenities[j][0]-amenities[i][0])**2 + (amenities[j][1]-amenities[i][1])**2
-            if length > length_max:
-                length_max = length
+        for bx, by in amenities:
+            if ax!=bx or ay!=by:
+                length_max = max(length_max, (bx-ax)**2+(by-ay)**2)
         if length_max < length_min:
             length_min = length_max
-            ans = tmp
+            ans = (ax, ay)
     print(*ans)
 
 if __name__ == "__main__":
