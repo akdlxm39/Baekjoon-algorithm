2e460f10ada84efd796285c9bde6c80e2c0b7bd8
Baek Jiung <zxcwldnd@gmail.com>
2025-04-03 17:06:03 +0900

    [Gold I] Title: 이사, Time: 260 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/17371. 이사/이사.py
+++ 백준/Gold/17371. 이사/이사.py
@@ -4,14 +4,10 @@ input = sys.stdin.readline
 def main():
     n = int(input())
     amenities = set(tuple(map(int, input().split())) for _ in range(n))
+    ans = (0, 0)
     length_min = 1000000000000
-    ans = amenities.pop()
-    amenities.add(ans)
     for ax, ay in amenities:
-        length_max = 0
-        for bx, by in amenities:
-            if ax!=bx or ay!=by:
-                length_max = max(length_max, (bx-ax)**2+(by-ay)**2)
+        length_max = max((bx-ax)**2+(by-ay)**2 for bx, by in amenities)
         if length_max < length_min:
             length_min = length_max
             ans = (ax, ay)
