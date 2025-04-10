44f9bca5d34dea8871f3d9c6e5d32a4b7203c2b8
Baek Jiung <zxcwldnd@gmail.com>
2025-04-10 23:09:22 +0900

    [Gold III] Title: 집으로, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1069. 집으로/집으로.py
+++ 백준/Gold/1069. 집으로/집으로.py
@@ -4,8 +4,7 @@ input = sys.stdin.readline
 def main():
     x, y, d, t = map(int, input().split())
     l = (x*x+y*y)**0.5
-    n = l//d
-    ans = min(n*t+l%d, (n+1)*t, l) if n else min(t+d-l, 2*t, l)
+    ans = min(l//d*t+l%d, (l//d+1)*t, l) if l>d else min(t+d-l, 2*t, l)
     print(ans)
 
 if __name__ == "__main__":
