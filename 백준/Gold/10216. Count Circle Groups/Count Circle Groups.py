0c36b3459c71a80e01cab2b82b1070bab205ede5
Baek Jiung <zxcwldnd@gmail.com>
2025-04-10 17:49:06 +0900

    [Gold IV] Title: Count Circle Groups, Time: 6656 ms, Memory: 131196 KB -BaekjoonHub

--- 백준/Gold/10216. Count Circle Groups/Count Circle Groups.py
+++ 백준/Gold/10216. Count Circle Groups/Count Circle Groups.py
@@ -1,30 +1,31 @@
-import sys, os, io
-from math import sqrt
-input = io.BytesIO(os.read(0,os.fstat(0).st_size)).readline
+import sys
+input = sys.stdin.readline
+
+def union(root, i, j):
+    root[find(root, i)] = find(root, j)
+
+def find(root, i):
+    if root[i] != i:
+        root[i] = find(root, root[i])
+    return root[i]
 
 def main():
     for _ in range(int(input())):
         n = int(input())
-        circle_group = {0:[tuple(map(int, input().split()))]}
+        circles = [tuple(map(int, input().split())) for _ in range(n)]
+        root = list(range(n))
         for i in range(1, n):
-            x1, y1, r1 = map(int, input().split())
-            flag = -1
-            for j, circles in tuple(circle_group.items()):
-                if flag == -1:
-                    for x2, y2, r2 in circles:
-                        if r1 + r2 >= sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)):
-                            circle_group[j].append((x1, y1, r1))
-                            flag = j
-                            break
-                else:
-                    for x2, y2, r2 in circles:
-                        if r1 + r2 >= sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)):
-                            circle_group[flag] += circle_group[j]
-                            del circle_group[j]
-                            break
-            if flag == -1:
-                circle_group[i] = [(x1, y1, r1)]
-        print(len(circle_group))
+            x1, y1, r1 = circles[i]
+            for j in range(i):
+                if find(root, i) == find(root, j):
+                    continue
+                x2, y2, r2 = circles[j]
+                if (r1 + r2) * (r1 + r2) >= (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1):
+                    union(root, i, j)
+        ans = 0
+        for i in range(n):
+            ans += i==root[i]
+        print(ans)
 
 if __name__ == "__main__":
     main()
