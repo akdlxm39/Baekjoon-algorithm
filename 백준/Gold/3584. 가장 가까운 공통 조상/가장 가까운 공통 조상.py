a4e56487df02edc9218864e6177f14fddb69e1df
Baek Jiung <zxcwldnd@gmail.com>
2025-04-15 16:14:35 +0900

    [Gold IV] Title: 가장 가까운 공통 조상, Time: 52 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.py
+++ 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.py
@@ -1,5 +1,4 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 def main():
@@ -7,27 +6,17 @@ def main():
     for _ in range(t):
         n = int(input())
         parents = [0] * (n + 1)
-        children = [[] for _ in range(n + 1)]
-        depths = [-1] * (n + 1)
         for _ in range(n - 1):
             a, b = map(int, input().split())
             parents[b] = a
-            children[a].append(b)
-        root = min(range(1, n + 1), key=lambda x: parents[x])
-        queue = deque([root])
-        depths[root] = 0
-        while queue:
-            cur = queue.popleft()
-            for nxt in children[cur]:
-                depths[nxt] = depths[cur] + 1
-                queue.append(nxt)
         x, y = map(int, input().split())
-        while x != y:
-            if depths[x] >= depths[y]:
-                x = parents[x]
-            else:
-                y = parents[y]
-        print(x)
+        route = [False] * (n + 1)
+        while x > 0:
+            route[x] = True
+            x = parents[x]
+        while not route[y]:
+            y = parents[y]
+        print(y)
 
 if __name__ == "__main__":
     main()
