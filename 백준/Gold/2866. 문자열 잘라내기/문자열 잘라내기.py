1f664504c1ca79d1f6ac5aa85f9c6e6765bf977b
Baek Jiung <zxcwldnd@gmail.com>
2025-04-20 18:11:09 +0900

    [Gold V] Title: 문자열 잘라내기, Time: 104 ms, Memory: 34472 KB -BaekjoonHub

--- 백준/Gold/2866. 문자열 잘라내기/문자열 잘라내기.py
+++ 백준/Gold/2866. 문자열 잘라내기/문자열 잘라내기.py
@@ -3,14 +3,21 @@ input = sys.stdin.readline
 
 def main():
     r, c = map(int, input().split())
-    strings = [input().rstrip() for _ in range(r)]
-    cstrings = list(strings[-1])
-    idx = r-1
-    while idx > 0 and len(set(cstrings)) != c:
-        idx -= 1
-        for i, char in enumerate(strings[idx]):
-            cstrings[i] += char
-    print(idx)
+    words = [input().rstrip() for _ in range(r)]
+    cwords = sorted(''.join(words[i][j] for i in range(r)) for j in range(c))
+    l, r = 0, r-1
+    while l <= r:
+        m = (l+r)//2
+        word_set = set()
+        for i in range(c):
+            word = cwords[i][m:]
+            if word in word_set:
+                r = m-1
+                break
+            word_set.add(word)
+        else:
+            l = m+1
+    print(r)
 
 if __name__ == "__main__":
     main()
