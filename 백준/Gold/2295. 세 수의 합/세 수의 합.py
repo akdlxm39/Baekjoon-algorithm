f1da218e532f17e2cccfeb599aeb3a67a6a82ea4
Baek Jiung <zxcwldnd@gmail.com>
2025-04-23 17:42:56 +0900

    [Gold IV] Title: 세 수의 합, Time: 156 ms, Memory: 67232 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.py
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.py
@@ -9,11 +9,11 @@ def main():
     for i in range(n):
         for j in range(i, n):
             xy_set.add(nums[i]+nums[j])
-    for i in range(n):
-        for j in range(i+1):
+    for i in range(n-1, 0, -1):
+        for j in range(i):
             if nums[i]-nums[j] in xy_set:
-                ans = nums[i]
-    print(ans)
+                print(nums[i])
+                return
 
 if __name__ == "__main__":
     main()
