8ea758a5d11d4a4e16dd6f867222c2578e9d8a76
Baek Jiung <zxcwldnd@gmail.com>
2025-04-23 17:46:54 +0900

    [Gold IV] Title: 세 수의 합, Time: 136 ms, Memory: 67232 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.py
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.py
@@ -1,14 +1,11 @@
 import sys
+from itertools import combinations_with_replacement
 input = sys.stdin.readline
 
 def main():
     n = int(input())
     nums = sorted(int(input()) for _ in range(n))
-    xy_set = set(nums)
-    ans = 0
-    for i in range(n):
-        for j in range(i, n):
-            xy_set.add(nums[i]+nums[j])
+    xy_set = {x+y for x, y in combinations_with_replacement(nums, 2)}
     for i in range(n-1, 0, -1):
         for j in range(i):
             if nums[i]-nums[j] in xy_set:
