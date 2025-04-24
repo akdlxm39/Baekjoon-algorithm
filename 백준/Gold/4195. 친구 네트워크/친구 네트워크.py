c7fb47481d76a33350c5e579469b6d915888531f
Baek Jiung <zxcwldnd@gmail.com>
2025-04-24 13:07:40 +0900

    [Gold II] Title: 친구 네트워크, Time: 208 ms, Memory: 54940 KB -BaekjoonHub

--- 백준/Gold/4195. 친구 네트워크/친구 네트워크.py
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.py
@@ -1,47 +1,31 @@
 import sys
-from copyreg import constructor
-
 input = sys.stdin.readline
 
-def union(root, num_of_friends, a, b):
-    ar, br = find(root, a), find(root, b)
-    if ar < br:
-        root[br] = ar
-        num_of_friends[ar] += num_of_friends[br]
-        return num_of_friends[ar]
-    elif ar > br:
-        root[ar] = br
-        num_of_friends[br] += num_of_friends[ar]
-    return num_of_friends[br]
+def union(parent, a, b):
+    root_a, root_b = find(parent, a), find(parent, b)
+    if root_a != root_b:
+        parent[root_a] += parent[root_b]
+        parent[root_b] = root_a
+    return parent[root_a]
 
-def find(root, x):
-    if root[x] != x:
-        root[x] = find(root, root[x])
-    return root[x]
 
-def new_friend(a, b, user_id, id_nums, root, num_of_friends):
-    if a not in user_id:
-        user_id[a] = id_nums
-        root.append(id_nums)
-        num_of_friends.append(1)
-        id_nums += 1
-    if b not in user_id:
-        user_id[b] = id_nums
-        root.append(id_nums)
-        num_of_friends.append(1)
-        id_nums += 1
-    return id_nums
+def find(parent, x):
+    if isinstance(parent[x], str):
+        parent[x] = find(parent, parent[x])
+        return parent[x]
+    else:
+        return x
 
 def main():
     t = int(input())
     for _ in range(t):
         f = int(input())
-        user_id, id_nums = dict(), 0
-        root, num_of_friends = [], []
+        parent = dict()
         for _ in range(f):
             a, b = input().split()
-            id_nums = new_friend(a, b, user_id, id_nums, root, num_of_friends)
-            print(union(root, num_of_friends, user_id[a], user_id[b]))
+            parent.setdefault(a, 1)
+            parent.setdefault(b, 1)
+            print(union(parent, a, b))
 
 if __name__ == "__main__":
     main()
