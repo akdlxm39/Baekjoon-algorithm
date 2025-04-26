a33b73e2fc732d0e73b72fcc612a4ee3048bd8ab
Baek Jiung <zxcwldnd@gmail.com>
2025-04-26 17:11:56 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 5056 ms, Memory: 624084 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.py
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.py
@@ -1,18 +1,22 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 def main():
     n, l = map(int, input().split())
     nums = list(map(int, input().split()))
-    dq = deque()
+    num_dq = [0]*n
+    idx_dq = [0]*n
+    front = 0
+    back = 0
     for i, num in enumerate(nums):
-        while dq and num <= dq[-1][0]:
-            dq.pop()
-        dq.append((num, i+l-1))
-        print(dq[0][0])
-        if dq[0][1] == i:
-            dq.popleft()
+        while back > front and num <= num_dq[back-1]:
+            back -= 1
+        num_dq[back] = num
+        idx_dq[back] = i+l-1
+        back += 1
+        print(num_dq[front], end=' ')
+        if idx_dq[front] == i:
+            front += 1
 
 if __name__ == "__main__":
     main()
