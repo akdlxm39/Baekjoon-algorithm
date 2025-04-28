63dbb904779bcf01cb91b8d8a344a5aa7d6d37b7
Baek Jiung <zxcwldnd@gmail.com>
2025-04-28 15:12:45 +0900

    [Gold IV] Title: 인구 이동, Time: 156 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -1,51 +1,50 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
-def union_move(n, l, r, countries, visited, i, j):
-    queue = deque([(i, j)])
-    union = []
-    union_cnt = 0
+def union_move(n, l, r, countries, visited, i, j, day):
+    union = [(i, j)]
+    visited[i][j] = day
+    union_idx = 0
+    union_len = 1
     population = 0
-    while queue:
-        x, y = queue.popleft()
-        union.append((x, y))
-        union_cnt += 1
+    while union_idx < union_len:
+        x, y = union[union_idx]
         population += countries[x][y]
         for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
             nx, ny = x + dx, y + dy
-            if not (0 <= nx < n and 0 <= ny < n) or visited[nx][ny]:
+            if not (0 <= nx < n and 0 <= ny < n) or visited[nx][ny]==day:
                 continue
             if not (l <= abs(countries[x][y] - countries[nx][ny]) <= r):
                 continue
-            visited[nx][ny] = True
-            queue.append((nx, ny))
-    if union_cnt == 1:
-        return False
-    population //= union_cnt
-    for x, y in union:
-        countries[x][y] = population
-    return True
+            visited[nx][ny] = day
+            union.append((nx, ny))
+            union_len += 1
+        union_idx += 1
+    if union_len > 1:
+        population //= union_idx
+        for x, y in union:
+            countries[x][y] = population
+        return union
+    return []
 
-def day(n, l, r, countries):
-    visited = [[False] * n for _ in range(n)]
-    flag = False
-    for i in range(n):
-        for j in range(n):
-            if visited[i][j]:
+def day_of_moves(n, l, r, countries):
+    day = -1
+    queue = [(i, j) for i in range(n) for j in range(i%2, n, 2)]
+    visited = [[-1] * n for _ in range(n)]
+    while queue:
+        day += 1
+        nxt_queue = []
+        for i, j in queue:
+            if visited[i][j] == day:
                 continue
-            visited[i][j] = True
-            if union_move(n, l, r, countries, visited, i, j):
-                flag = True
-    return flag
+            nxt_queue += union_move(n, l, r, countries, visited, i, j, day)
+        queue = nxt_queue
+    return day
 
 def main():
     n, l, r = map(int, input().split())
     countries = [list(map(int, input().split())) for _ in range(n)]
-    day_cnt = 0
-    while day(n, l, r, countries):
-        day_cnt += 1
-    print(day_cnt)
+    print(day_of_moves(n, l, r, countries))
 
 if __name__ == "__main__":
     main()
