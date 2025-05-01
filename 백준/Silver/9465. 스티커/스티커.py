460dc8e87c419d1a874505b13f1b44d1cbd0da12
Baek Jiung <zxcwldnd@gmail.com>
2025-05-01 16:24:28 +0900

    [Silver I] Title: 스티커, Time: 348 ms, Memory: 41492 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -6,12 +6,12 @@ def main():
     for _ in range(t):
         n = int(input())
         stickers = [list(map(int, input().split())) for _ in range(2)]
-        dp = [[stickers[0][j], stickers[1][j], 0] for j in range(n)]
+        top, bottom, non = stickers[0][0], stickers[1][0], 0
         for i in range(1, n):
-            dp[i][0] += max(dp[i-1][1], dp[i-1][2])
-            dp[i][1] += max(dp[i-1][0], dp[i-1][2])
-            dp[i][2] += max(dp[i-1][0], dp[i-1][1])
-        print(max(dp[-1]))
+            top, bottom, non = (stickers[0][i] + max(bottom, non),
+                                stickers[1][i] + max(top, non),
+                                max(top, bottom))
+        print(max(top, bottom, non))
 
 if __name__ == "__main__":
     main()
