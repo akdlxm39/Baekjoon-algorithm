206f109817597b95dc62c9b80e72cdfd6280e713
Baek Jiung <zxcwldnd@gmail.com>
2025-05-08 15:40:07 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 612 ms, Memory: 161400 KB -BaekjoonHub

--- 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.py
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.py
@@ -4,17 +4,14 @@ input = sys.stdin.readline
 
 def main():
     n = int(input())
-    nums = list(map(int, input().split()))
-    ans_list = [nums[0]]
-    ans = 1
-    for i in range(1, n):
-        if ans_list[-1] < nums[i]:
-            ans_list.append(nums[i])
-            ans += 1
+    x, *nums = map(int, input().split())
+    ans_list = [x]
+    for a in nums:
+        if ans_list[-1] < a:
+            ans_list.append(a)
         else:
-            ans_list[bisect_left(ans_list, nums[i])] = nums[i]
-    print(ans)
-
+            ans_list[bisect_left(ans_list, a)] = a
+    print(len(ans_list))
 
 if __name__ == "__main__":
     main()
