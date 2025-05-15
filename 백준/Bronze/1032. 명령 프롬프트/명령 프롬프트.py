76c194d234b53528ce508229c9f02e92318d16a1
Baek Jiung <zxcwldnd@gmail.com>
2025-05-15 18:30:13 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
@@ -3,10 +3,10 @@ input = sys.stdin.readline
 
 def main():
     n = int(input())
-    ans = list(input().rstrip())
+    ans = list(input())
     size = len(ans)
     for _ in range(n-1):
-        word = input().rstrip()
+        word = input()
         for i in range(size):
             if ans[i] != word[i]:
                 ans[i] = '?'
