eef505674936c29895df0b6ddc845e8ab4328365
Baek Jiung <zxcwldnd@gmail.com>
2025-05-15 16:43:24 +0900

    [Gold IV] Title: 플로이드, Time: 236 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+def main():
+    n, m = int(input()), int(input())
+    adj_matrix = [[INF]*n for _ in range(n)]
+    for i in range(n):
+        adj_matrix[i][i] = 0
+    for _ in range(m):
+        s, e, cost = map(int, input().split())
+        adj_matrix[s-1][e-1] = min(adj_matrix[s-1][e-1], cost)
+    for k in range(n):
+        for i in range(n):
+            if i == k: continue
+            for j in range(n):
+                if j == k or j == i: continue
+                adj_matrix[i][j] = min(adj_matrix[i][j], adj_matrix[i][k] + adj_matrix[k][j])
+    for line in adj_matrix:
+        print(' '.join(map(lambda x: str(x) if x != INF else '0', line)))
+
+if __name__ == "__main__":
+    main()
