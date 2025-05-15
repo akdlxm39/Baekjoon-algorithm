4ef27116199703732908bfa3addf831c9aed4dde
Baek Jiung <zxcwldnd@gmail.com>
2025-05-15 16:26:48 +0900

    [Gold V] Title: 최소비용 구하기, Time: 148 ms, Memory: 40304 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,27 +1,27 @@
 import sys
 from heapq import heappush, heappop
 input = sys.stdin.readline
+INF = int(1e9)
 
 def main():
     n, m = int(input()), int(input())
-    adj_list = [[] for _ in range(n+1)]
+    adj_list = [dict() for _ in range(n+1)]
     for _ in range(m):
         s, e, cost = map(int, input().split())
-        adj_list[s].append((e, cost))
+        adj_list[s][e] = min(adj_list[s].get(e, INF), cost)
     start, end = map(int, input().split())
-    costs = [-1] * (n+1)
+    costs = [INF] * (n+1)
     heap = [(0, start)]
+    costs[start] = 0
     while heap:
         cost, cur = heappop(heap)
-        if costs[cur] != -1:
-            continue
-        costs[cur] = cost
         if cur == end:
             break
-        for nxt, nxt_cost in adj_list[cur]:
-            if costs[nxt] != -1:
-                continue
-            heappush(heap, (cost + nxt_cost, nxt))
+        for nxt, nxt_cost in adj_list[cur].items():
+            nxt_cost += cost
+            if costs[nxt] > nxt_cost:
+                costs[nxt] = nxt_cost
+                heappush(heap, (nxt_cost, nxt))
     print(costs[end])
 
 if __name__ == "__main__":
