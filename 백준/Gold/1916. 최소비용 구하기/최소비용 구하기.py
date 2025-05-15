b308c6f0679e6784485ab61d44e79d179ea661d4
Baek Jiung <zxcwldnd@gmail.com>
2025-05-15 16:15:49 +0900

    [Gold V] Title: 최소비용 구하기, Time: 240 ms, Memory: 58232 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -0,0 +1,26 @@
+import sys
+from heapq import heappush, heappop
+input = sys.stdin.readline
+
+def main():
+    n, m = int(input()), int(input())
+    adj_list = [[] for _ in range(n+1)]
+    for _ in range(m):
+        s, e, cost = map(int, input().split())
+        adj_list[s].append((e, cost))
+    start, end = map(int, input().split())
+    costs = [-1] * (n+1)
+    heap = [(0, start)]
+    while heap:
+        cost, cur = heappop(heap)
+        if costs[cur] != -1:
+            continue
+        costs[cur] = cost
+        for nxt, nxt_cost in adj_list[cur]:
+            if costs[nxt] != -1:
+                continue
+            heappush(heap, (cost + nxt_cost, nxt))
+    print(costs[end])
+
+if __name__ == "__main__":
+    main()
