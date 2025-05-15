f716e3d39ef00c55c6751388f97b2ed119028dd7
Baek Jiung <zxcwldnd@gmail.com>
2025-05-15 18:16:40 +0900

    [Gold IV] Title: 뱀, Time: 36 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -1,33 +1,29 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
 def main():
-    n = int(input())+2
-    board = [[0]*n for _ in range(n)]
-    moves = dict()
-    for i in range(n):
-        board[0][i] = board[n-1][i] = board[i][0] = board[i][n-1] = 1
+    n = int(input())
+    apples = [[False]*(n+1) for _ in range(n+1)]
     for _ in range(int(input())):
         x, y = map(int, input().split())
-        board[x][y] = 2
+        apples[x][y] = True
+    moves = dict()
     for _ in range(int(input())):
         x, c = input().split()
         moves[int(x)] = c
-    snake = deque([(1, 1)])
-    board[1][1] = 1
-    sec, dir = 1, 0
+    snake = [(1, 1)]
+    tail, sec, dir = 0, 1, 0
     while True:
-        nx, ny = snake[0][0] + dx[dir], snake[0][1] + dy[dir]
-        if board[nx][ny] == 1:
+        nx, ny = snake[-1][0] + dx[dir], snake[-1][1] + dy[dir]
+        if not (0 < nx <= n and 0 < ny <= n) or (nx, ny) in snake[tail:]:
             break
-        if board[nx][ny] != 2:
-            rx, ry = snake.pop()
-            board[rx][ry] = 0
-        snake.appendleft((nx, ny))
-        board[nx][ny] = 1
+        if apples[nx][ny]:
+            apples[nx][ny] = False
+        else:
+            tail += 1
+        snake.append((nx, ny))
         if sec in moves:
             dir = (dir + (1 if moves[sec] == 'D' else -1)) % 4
         sec += 1
