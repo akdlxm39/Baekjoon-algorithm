4b11fe873c8567a02701705259ed513dc1d16b71
Baek Jiung <zxcwldnd@gmail.com>
2025-05-17 16:36:58 +0900

    [Platinum V] Title: 공장, Time: 1940 ms, Memory: 120348 KB -BaekjoonHub

--- 백준/Platinum/7578. 공장/공장.py
+++ 백준/Platinum/7578. 공장/공장.py
@@ -1,40 +1,27 @@
 import sys
 input = sys.stdin.readline
 
-def update(tree, node, left, right, idx):
-    tree[node] += 1
-    if left == right:
-        return
-    mid = (left + right) // 2
-    if left <= idx <= mid:
-        update(tree, node*2, left, mid, idx)
-    else:
-        update(tree, node*2+1, mid+1, right, idx)
+def update(n, tree, i):
+    while i <= n:
+        tree[i] += 1
+        i += i & -i
 
-def sum(tree, node, left, right, start, end):
-    if end < left or right < start:
-        return 0
-    if start <= left and right <= end:
-        return tree[node]
-    mid = (left + right) // 2
-    return (sum(tree, node*2, left, mid, start, end)
-            + sum(tree, node*2+1, mid+1, right, start, end))
+def sum(tree, i):
+    res = 0
+    while i > 0:
+        res += tree[i]
+        i -= i & -i
+    return res
 
 def main():
     n = int(input())
+    mapping = {m:i for i, m in enumerate(map(int, input().split()), 1)}
+    list = [mapping[m] for m in map(int, input().split())]
+    tree = [0] * (n + 1)
     ans = 0
-    tree = [0] * (n * 4)
-    mapping = {}
-    for i, m in enumerate(map(int, input().split()), 1):
-        mapping[m] = i
-    list = []
-    for i, m in enumerate(map(int, input().split()), 1):
-        list.append((mapping[m], i))
-    list.sort()
-    for _, i in list:
-        if i < n:
-            ans += sum(tree, 1, 1, n, i+1, n)
-        update(tree, 1, 1, n, i)
+    for i in reversed(list):
+        ans += sum(tree, i)
+        update(n, tree, i)
     print(ans)
 
 if __name__ == "__main__":
