4d0eacc44eac7f43c5912668a2d0484acd57e79f
Baek Jiung <zxcwldnd@gmail.com>
2025-05-23 14:42:50 +0900

    [Silver IV] Title: 좋은 구간, Time: 40 ms, Memory: 34476 KB -BaekjoonHub

--- 백준/Silver/1059. 좋은 구간/좋은 구간.py
+++ 백준/Silver/1059. 좋은 구간/좋은 구간.py
@@ -8,11 +8,10 @@ def main():
     n = int(input())
     i = bisect_left(s, n)
     if s[i] == n:
-        ans = 0
+        print(0)
     else:
         left, right = s[i-1], s[i]
-        ans = (n-left)*(right-n)-1
-    print(ans)
+        print((n-left)*(right-n)-1)
 
 if __name__ == "__main__":
     main()
