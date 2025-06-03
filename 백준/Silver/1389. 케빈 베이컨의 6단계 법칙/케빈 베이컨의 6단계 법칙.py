5f716fcc9d4e9f54524ad731a7583611246b6ac9
Baek Jiung <zxcwldnd@gmail.com>
2025-06-03 16:05:02 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 104 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -4,23 +4,23 @@ INF = int(1e9)
 
 def main():
     n, m = map(int, input().split())
-    adj_list = [[INF]*(n+1) for _ in range(n+1)]
+    friends = [[INF]*(n+1) for _ in range(n+1)]
     for i in range(1, n+1):
-        adj_list[i][i] = 0
+        friends[i][i] = 0
     for _ in range(m):
         a, b = map(int, input().split())
-        adj_list[a][b] = 1
-        adj_list[b][a] = 1
+        friends[a][b] = 1
+        friends[b][a] = 1
     for k in range(1, n+1):
         for i in range(1, n+1):
             for j in range(1, n+1):
-                tmp = adj_list[i][k] + adj_list[k][j]
-                if tmp < adj_list[i][j]:
-                    adj_list[i][j] = tmp
-    
+                tmp = friends[i][k] + friends[k][j]
+                if tmp < friends[i][j]:
+                    friends[i][j] = tmp
+
     ans, ans_bacon = 0, INF
     for cur in range(1, n+1):
-        cur_bacon = sum(adj_list[cur][1:])
+        cur_bacon = sum(friends[cur][1:])
         if cur_bacon < ans_bacon:
             ans = cur
             ans_bacon = cur_bacon
