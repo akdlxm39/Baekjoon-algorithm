cef0cb435b969e5a23369a0592e3ad683610188f
Baek Jiung <zxcwldnd@gmail.com>
2025-06-03 16:32:18 +0900

    [Bronze V] Title: 긴자리 계산, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+def main():
+    a, b = int(input()), int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
+
+if __name__ == "__main__":
+    main()
