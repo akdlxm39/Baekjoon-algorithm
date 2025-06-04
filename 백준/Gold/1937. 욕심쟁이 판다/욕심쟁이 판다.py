eb0326faf371734f89759eaa0fec043d38c8f661
Baek Jiung <zxcwldnd@gmail.com>
2025-06-04 13:00:14 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 740 ms, Memory: 52476 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
@@ -1,27 +1,27 @@
 import sys
 sys.setrecursionlimit(30000)
 input = sys.stdin.readline
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
 
 def eat(n, bamboos, memo, x, y):
     if memo[x][y]:
         return memo[x][y]
     res = 0
-    for i in range(4):
-        nx, ny = x+dx[i], y+dy[i]
-        if 0 <= nx < n and 0 <= ny < n and bamboos[nx][ny] < bamboos[x][y]:
+    for dx, dy in [(0,1), (1,0), (0,-1), (-1,0)]:
+        nx, ny = x+dx, y+dy
+        if bamboos[nx][ny] > bamboos[x][y]:
             res = max(res, eat(n, bamboos, memo, nx, ny))
     memo[x][y] = res+1
     return memo[x][y]
 
 def main():
     n = int(input())
-    bamboos = [list(map(int, input().split())) for _ in range(n)]
-    memo = [[0]*n for _ in range(n)]
+    bamboos = ([[0]*(n+2)]+
+               [[0]+list(map(int, input().split()))+[0] for _ in range(n)]+
+               [[0]*(n+2)])
+    memo = [[0]*(n+2) for _ in range(n+2)]
     ans = 0
-    for i in range(n):
-        for j in range(n):
+    for i in range(1,n+1):
+        for j in range(1,n+1):
             ans = max(ans, eat(n, bamboos, memo, i, j))
     print(ans)
 
