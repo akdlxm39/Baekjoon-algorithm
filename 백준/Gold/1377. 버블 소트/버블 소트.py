d7995637836e21c74517568bf576279475d628ce
Baek Jiung <zxcwldnd@gmail.com>
2025-06-15 14:37:33 +0900

    [Gold II] Title: 버블 소트, Time: 480 ms, Memory: 81904 KB -BaekjoonHub

--- 백준/Gold/1377. 버블 소트/버블 소트.py
+++ 백준/Gold/1377. 버블 소트/버블 소트.py
@@ -4,8 +4,8 @@ input = sys.stdin.readline
 def main():
     n = int(input())
     nums = [int(input()) for _ in range(n)]
-    sort_idx = sorted(range(n), key=lambda i: nums[i])
-    ans = max(j-i for i, j in enumerate(sort_idx))+1
+    idx = sorted(range(n), key=lambda i: nums[i])
+    ans = max(i-j for j, i in enumerate(idx))+1
     print(ans)
 
 if __name__ == "__main__":
