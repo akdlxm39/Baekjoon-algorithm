f331fce05d78f1e9ec96830e7aa57d76550b265a
Baek Jiung <zxcwldnd@gmail.com>
2025-06-15 15:32:21 +0900

    [Gold I] Title: 부분수열의 합 2, Time: 888 ms, Memory: 163860 KB -BaekjoonHub

--- 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.py
+++ 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.py
@@ -1,22 +1,20 @@
 import sys
+from collections import Counter
 input = sys.stdin.readline
 
-def all_kind_sum(size, sub, sum_sub, cur, idx):
-    if idx == size:
-        sum_sub[cur] = sum_sub.get(cur, 0) + 1
-        return
-    all_kind_sum(size, sub, sum_sub, cur, idx+1)
-    all_kind_sum(size, sub, sum_sub, cur+sub[idx], idx+1)
+def all_kind_sum(sub):
+    res = [0]
+    for num in sub:
+        tmp = [num+x for x in res]
+        res += tmp
+    return Counter(res[1:])
 
 def main():
     n, s = map(int, input().split())
     nums = list(map(int, input().split()))
     left_sub, right_sub = nums[:n//2], nums[n//2:]
-    left_sum, right_sum = {}, {}
-    all_kind_sum(len(left_sub), left_sub, left_sum, 0, 0)
-    all_kind_sum(len(right_sub), right_sub, right_sum, 0, 0)
-    left_sum[0] -= 1
-    right_sum[0] -= 1
+    left_sum = all_kind_sum(left_sub)
+    right_sum = all_kind_sum(right_sub)
     ans = left_sum.get(s, 0) + right_sum.get(s, 0)
     for left in left_sum:
         right = s - left
