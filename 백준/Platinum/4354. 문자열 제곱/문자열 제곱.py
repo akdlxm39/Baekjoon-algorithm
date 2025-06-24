9870b021804c241fcea2bf585876812b7c1ec658
Baek Jiung <zxcwldnd@gmail.com>
2025-06-24 16:59:21 +0900

    [Platinum V] Title: 문자열 제곱, Time: 104 ms, Memory: 36316 KB -BaekjoonHub

--- 백준/Platinum/4354. 문자열 제곱/문자열 제곱.py
+++ 백준/Platinum/4354. 문자열 제곱/문자열 제곱.py
@@ -1,24 +1,12 @@
 import sys
 input = sys.stdin.readline
 
-def getPi(p):
-    pi = [0]*len(p)
-    j = 0
-    for i in range(1, len(p)):
-        while j > 0 and p[i] != p[j]:
-            j = pi[j-1]
-        if p[i] == p[j]:
-            pi[i] = j = j + 1
-    return pi
-
 def main():
     while True:
         s = input().rstrip()
         if s == '.':
             break
-        pi = getPi(s)
-        ans = len(s)/(len(s) - pi[-1])
-        print(int(ans) if ans%1==0 else 1)
+        print(len(s)//(s+s).find(s,1))
 
 if __name__ == "__main__":
     main()
