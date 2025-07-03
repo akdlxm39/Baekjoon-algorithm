458898f4f658ae010ca7c22a16b068ff6f3cbd95
Baek Jiung <zxcwldnd@gmail.com>
2025-07-04 00:18:37 +0900

    [Silver II] Title: 유기농 배추, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -2,33 +2,28 @@ import sys
 input = sys.stdin.readline
 dxdy = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-def bfs(n, m, map_, x, y):
-    queue = [(x, y)]
-    while queue:
-        nxt_queue = []
-        for cx, cy in queue:
-            for dx, dy in dxdy:
-                nx, ny = cx + dx, cy + dy
-                if not (0 <= nx < n and 0 <= ny < m) or map_[nx][ny]==0: continue
-                map_[nx][ny] = 0
-                nxt_queue.append((nx, ny))
-        queue = nxt_queue
+def bfs(cabbages):
+    count = 0
+    while cabbages:
+        queue = [cabbages.pop()]
+        while queue:
+            nxt_queue = []
+            for cx, cy in queue:
+                for dx, dy in dxdy:
+                    nx, ny = cx + dx, cy + dy
+                    if (nx, ny) in cabbages:
+                        cabbages.remove((nx, ny))
+                        nxt_queue.append((nx, ny))
+            queue = nxt_queue
+        count += 1
+    return count
 
 def main():
     t = int(input())
     for _ in range(t):
         m, n, k = map(int, input().split())
-        map_ = [[False]*m for _ in range(n)]
-        for _ in range(k):
-            x, y = map(int, input().split())
-            map_[y][x] = True
-        ans = 0
-        for i in range(n):
-            for j in range(m):
-                if map_[i][j] == 0: continue
-                bfs(n, m, map_, i, j)
-                ans += 1
-        print(ans)
+        cabbages = set(tuple(map(int, input().split())) for _ in range(k))
+        print(bfs(cabbages))
 
 if __name__ == "__main__":
     main()
