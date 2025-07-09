214e6955069a9793aa1ad28f41a0ba30841530fe
Baek Jiung <zxcwldnd@gmail.com>
2025-07-09 16:39:13 +0900

    [Gold II] Title: 컵라면, Time: 288 ms, Memory: 50972 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.py
+++ 백준/Gold/1781. 컵라면/컵라면.py
@@ -1,15 +1,20 @@
 import sys
-from heapq import heappush, heappop
+from heapq import heappush, heappop, heapify
 input = sys.stdin.readline
 
 def main():
     n = int(input())
-    problems = sorted((tuple(map(int, input().split())) for _ in range(n)), reverse=True)
+    problems = {}
+    for _ in range(n):
+        deadline, reward = map(int, input().split())
+        problems.setdefault(deadline, []).append(reward)
+    deadlines = sorted(problems.keys(), reverse=True)
     heap = []
     ans = idx = 0
     for i in range(n, 0, -1):
-        while idx < n and problems[idx][0] >= i:
-            heappush(heap, -problems[idx][1])
+        while idx < len(deadlines) and deadlines[idx] >= i:
+            for reward in problems[deadlines[idx]]:
+                heappush(heap, -reward)
             idx += 1
         if heap:
             ans -= heappop(heap)
