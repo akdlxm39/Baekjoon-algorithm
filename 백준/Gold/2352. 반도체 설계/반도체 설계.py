b3a8fb4062353fb860beb950960155c38851aa9c
Baek Jiung <zxcwldnd@gmail.com>
2025-07-13 15:34:28 +0900

    [Gold II] Title: 반도체 설계, Time: 48 ms, Memory: 37892 KB -BaekjoonHub

--- 백준/Gold/2352. 반도체 설계/반도체 설계.py
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.py
@@ -3,15 +3,14 @@ from bisect import bisect_left
 input = sys.stdin.readline
 
 def main():
-    n = int(input())
-    nums = list(map(int, input().split()))
-    stack = []
-    for num in nums:
-        if not stack or num > stack[-1]:
+    _ = int(input())
+    stack = [0]
+    for num in map(int, input().split()):
+        if num > stack[-1]:
             stack.append(num)
         else:
             stack[bisect_left(stack, num)] = num
-    print(len(stack))
+    print(len(stack) - 1)
 
 if __name__ == "__main__":
     main()
