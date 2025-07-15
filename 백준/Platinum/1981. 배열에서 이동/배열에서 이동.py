6936a4441474902a9feea205cce228682a0d8b26
Baek Jiung <zxcwldnd@gmail.com>
2025-07-15 17:08:26 +0900

    [Platinum V] Title: 배열에서 이동, Time: 436 ms, Memory: 35004 KB -BaekjoonHub

--- 백준/Platinum/1981. 배열에서 이동/배열에서 이동.py
+++ 백준/Platinum/1981. 배열에서 이동/배열에서 이동.py
@@ -1,29 +1,36 @@
 import sys
 from collections import deque
-
 input = sys.stdin.readline
 dx, dy = [0, 0, 1, -1], [1, -1, 0, 0]
 
-def bfs(n, arr):
-    queue = deque([(0, 0, arr[0][0], arr[0][0])])
-    visited = [[[201]*201 for _ in range(n)] for _ in range(n)]
-    visited[0][0][arr[0][0]] = arr[0][0]
+def bfs(n, arr, snum, enum):
+    queue = deque([(0, 0)])
+    visited = [[False] * n for _ in range(n)]
+    visited[0][0] = True
     while queue:
-        cx, cy, cmin, cmax = queue.popleft()
-        if visited[cx][cy][cmin] < cmax: continue
+        cx, cy = queue.popleft()
         for i in range(4):
             nx, ny = cx + dx[i], cy + dy[i]
-            if not (0 <= nx < n and 0 <= ny < n): continue
-            nmin, nmax = min(cmin, arr[nx][ny]), max(cmax, arr[nx][ny])
-            if visited[nx][ny][nmin] <= nmax: continue
-            visited[nx][ny][nmin] = nmax
-            queue.append((nx, ny, nmin, nmax))
-    return min(visited[-1][-1][i] - i for i in range(201) if visited[-1][-1][i] != 201)
+            if not (0 <= nx < n and 0 <= ny < n) or visited[nx][ny]: continue
+            visited[nx][ny] = True
+            if snum <= arr[nx][ny] <= enum:
+                queue.append((nx, ny))
+    return visited[-1][-1]
 
 def main():
     n = int(input())
     arr = [list(map(int, input().split())) for _ in range(n)]
-    print(bfs(n, arr))
+    start_num, end_num = min(arr[0][0], arr[-1][-1]), max(arr[0][0], arr[-1][-1])
+    left, right = end_num - start_num, 200
+    while left < right:
+        mid = (left + right) // 2
+        for snum in range(max(end_num - mid, 0), start_num + 1):
+            if bfs(n, arr, snum, snum + mid):
+                right = mid
+                break
+        else:
+            left = mid + 1
+    print(left)
 
 if __name__ == "__main__":
     main()
