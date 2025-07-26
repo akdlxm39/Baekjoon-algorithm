70796c8b0d45aacb378f78a7ae894cd4ebb394c8
Baek Jiung <zxcwldnd@gmail.com>
2025-07-26 14:31:15 +0900

    [Platinum V] Title: 약 팔기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def main():
+    n = int(input())
+    print(2000)
+    print('1 '*1000 + '1000 '*999 + '1000')
+
+
+if __name__ == "__main__":
+    main()
