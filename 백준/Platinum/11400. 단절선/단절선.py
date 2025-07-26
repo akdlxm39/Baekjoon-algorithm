eb0d0fa220477260253d03ccf2d49080e60cf090
Baek Jiung <zxcwldnd@gmail.com>
2025-07-26 20:12:34 +0900

    [Platinum IV] Title: 단절선, Time: 916 ms, Memory: 91304 KB -BaekjoonHub

---
+++ 백준/Platinum/11400. 단절선/단절선.py
@@ -0,0 +1,42 @@
+import sys
+
+input = sys.stdin.readline
+sys.setrecursionlimit(int(1e7))
+
+
+def dfs(adj_list, visited, cur, prev, idx, ans):
+    visited[cur] = idx
+    res = idx
+    for nxt in adj_list[cur]:
+        if nxt == prev: continue
+        if visited[nxt] == 0:
+            tmp, idx = dfs(adj_list, visited, nxt, cur, idx + 1, ans)
+            if visited[cur] < tmp:
+                ans.add((cur, nxt) if cur < nxt else (nxt, cur))
+            else:
+                res = min(res, tmp)
+        else:
+            res = min(res, visited[nxt])
+
+    return res, idx
+
+
+def main():
+    v, e = map(int, input().split())
+    adj_list = [[] for _ in range(v + 1)]
+    for _ in range(e):
+        a, b = map(int, input().split())
+        adj_list[a].append(b)
+        adj_list[b].append(a)
+    visited = [0] * (v + 1)
+    ans = set()
+    for i in range(1, v+1):
+        if not visited[i]:
+            dfs(adj_list, visited, 1, 0, 1, ans)
+    print(len(ans))
+    if len(ans):
+        print('\n'.join(map(lambda x: ' '.join(map(str, x)), sorted(ans))))
+
+
+if __name__ == "__main__":
+    main()
