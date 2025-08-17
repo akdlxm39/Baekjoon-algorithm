0e661cef83553d610d52e8de3b1cdd9f2caaf25e
Baek Jiung <zxcwldnd@gmail.com>
2025-08-17 17:41:26 +0900

    [Gold I] Title: 구슬 탈출, Time: 60 ms, Memory: 35044 KB -BaekjoonHub

--- 백준/Gold/13459. 구슬 탈출/구슬 탈출.py
+++ 백준/Gold/13459. 구슬 탈출/구슬 탈출.py
@@ -2,80 +2,54 @@ import sys
 from collections import deque
 
 input = sys.stdin.readline
-dictionary = {'.': 0, '#': 1, 'R': 2, 'B': 3, 'O': 4}
 dx, dy = [0, 0, 1, -1], [1, -1, 0, 0]
 
 
-def bfs(board, red, blue, goal):
-    queue = deque([(red, blue)])
-    visited = {(red, blue)}
+def move(board, x, y, dx_, dy_):
+    cnt = 0
+    while board[x + dx_][y + dy_] != '#' and board[x][y] != 'O':
+        x += dx_
+        y += dy_
+        cnt += 1
+    return cnt, x, y
+
+
+def bfs(board, start):
+    queue = [start]
+    visited = {start}
     for _ in range(10):
         nxt_queue = []
-        for (rx, ry), (bx, by) in queue:
+        for rx, ry, bx, by in queue:
             for i in range(4):
-                rcx, rcy, bcx, bcy = rx, ry, bx, by
-                bmove, state = True, 0
-                while True:
-                    bnx, bny = bcx + dx[i], bcy + dy[i]
-                    if board[bnx][bny]:
-                        bmove = False
-                        break
-                    elif bnx == goal[0] and bny == goal[1]:
-                        state = 1
-                        break
-                    elif bnx == rcx and bny == rcy:
-                        break
-                    bcx, bcy = bnx, bny
-                if state == 1: continue
-                while True:
-                    rnx, rny = rcx + dx[i], rcy + dy[i]
-                    if board[rnx][rny]:
-                        break
-                    elif rnx == goal[0] and rny == goal[1]:
-                        rcx, rcy = rnx, rny
-                        state = 2
-                        break
-                    elif rnx == bcx and rny == bcy:
-                        break
-                    rcx, rcy = rnx, rny
-                while bmove:
-                    bnx, bny = bcx + dx[i], bcy + dy[i]
-                    if board[bnx][bny]:
-                        break
-                    elif bnx == goal[0] and bny == goal[1]:
-                        state = 1
-                        break
-                    elif bnx == rcx and bny == rcy:
-                        break
-                    bcx, bcy = bnx, bny
-                if state == 1:
-                    continue
-                elif state == 2:
-                    return True
-                nxt = ((rcx, rcy), (bcx, bcy))
+                rcnt, rnx, rny = move(board, rx, ry, dx[i], dy[i])
+                bcnt, bnx, bny = move(board, bx, by, dx[i], dy[i])
+                if board[bnx][bny] == 'O': continue
+                if board[rnx][rny] == 'O': return 1
+                if rnx == bnx and rny == bny:
+                    if rcnt > bcnt:
+                        rnx -= dx[i]
+                        rny -= dy[i]
+                    else:
+                        bnx -= dx[i]
+                        bny -= dy[i]
+                nxt = (rnx, rny, bnx, bny)
                 if nxt in visited: continue
                 nxt_queue.append(nxt)
                 visited.add(nxt)
         queue = nxt_queue
-    return False
+    return 0
 
 
 def main():
     n, m = map(int, input().split())
-    board = []
-    red, blue, goal = None, None, None
+    board = [input().rstrip() for _ in range(n)]
+    start = [0, 0, 0, 0]
     for i in range(n):
-        line = []
-        for j, x in enumerate(input().rstrip()):
-            line.append(1 if x == '#' else 0)
-            if x == 'R':
-                red = (i, j)
-            elif x == 'B':
-                blue = (i, j)
-            elif x == 'O':
-                goal = (i, j)
-        board.append(line)
-    print(1 if bfs(board, red, blue, goal) else 0)
+        if 'R' in board[i]:
+            start[0], start[1] = i, board[i].index('R')
+        if 'B' in board[i]:
+            start[2], start[3] = i, board[i].index('B')
+    print(bfs(board, tuple(start)))
 
 
 if __name__ == "__main__":
