f6203b72c65c9b53f3658f38b9fcf232420995df
Baek Jiung <zxcwldnd@gmail.com>
2025-08-26 16:13:21 +0900

    [Platinum III] Title: 열혈강호 3, Time: 8592 ms, Memory: 42424 KB -BaekjoonHub

---
+++ 백준/Platinum/11377. 열혈강호 3/열혈강호 3.py
@@ -0,0 +1,40 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def assign(can_work, assigned, visited, cur):
+    for work in can_work[cur]:
+        if visited[work]: continue
+        visited[work] = True
+        if assigned[work] == -1 or assign(can_work, assigned, visited, assigned[work]):
+            assigned[work] = cur
+            return True
+    return False
+
+
+def main():
+    n, m, k = map(int, input().split())
+    can_work = [[] for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        cnt, *works = map(int, input().split())
+        if cnt:
+            can_work[i] += works
+    assigned = [-1] * (m + 1)
+    ans = 0
+    for i in range(1, n + 1):
+        visited = [False] * (m + 1)
+        if assign(can_work, assigned, visited, i):
+            ans += 1
+    for i in range(1, n + 1):
+        visited = [False] * (m + 1)
+        if assign(can_work, assigned, visited, i):
+            ans += 1
+            k -= 1
+            if not k:
+                break
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
