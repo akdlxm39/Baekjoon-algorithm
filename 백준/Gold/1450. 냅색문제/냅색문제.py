2cd7552069483b071f93114b07397c0c61f8257d
Baek Jiung <zxcwldnd@gmail.com>
2025-08-28 15:00:10 +0900

    [Gold I] Title: 냅색문제, Time: 68 ms, Memory: 36796 KB -BaekjoonHub

---
+++ 백준/Gold/1450. 냅색문제/냅색문제.py
@@ -0,0 +1,34 @@
+import sys
+from bisect import bisect_right
+from itertools import combinations
+
+input = sys.stdin.readline
+
+
+def all_kind_sum(sub):
+    res = []
+    for i in range(len(sub) + 1):
+        for x in combinations(sub, i):
+            res.append(sum(x))
+    return res
+
+
+def main():
+    n, c = map(int, input().split())
+    weights = [x for x in map(int, input().split()) if x <= c]
+    cnt = len(weights)
+    if cnt < 2:
+        print(cnt + 1)
+        return
+    left_sub, right_sub = weights[:cnt // 2], weights[cnt // 2:]
+    left_sum, right_sum = all_kind_sum(left_sub), all_kind_sum(right_sub)
+    right_sum.sort()
+    ans = 0
+    for l in left_sum:
+        max_r = c - l
+        ans += bisect_right(right_sum, max_r)
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
