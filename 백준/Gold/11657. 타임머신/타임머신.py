f80d12adb85420ad228b707a3b1b97137295b9c7
Baek Jiung <zxcwldnd@gmail.com>
2025-09-01 16:55:17 +0900

    [Gold IV] Title: 타임머신, Time: 252 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.py
+++ 백준/Gold/11657. 타임머신/타임머신.py
@@ -7,19 +7,26 @@ INF = int(1e9)
 def bellman_ford(n, edges):
     dist = [INF] * (n + 1)
     dist[1] = 0
-    for i in range(n - 1):
-        for cur, nxt, cost in edges:
-            if dist[cur] != INF:
-                dist[nxt] = min(dist[nxt], dist[cur] + cost)
-    for cur, nxt, cost in edges:
-        if dist[cur] != INF and dist[nxt] > dist[cur] + cost:
-            return -1
+    for _ in range(n - 1):
+        for cur in range(1, n + 1):
+            if dist[cur] == INF: continue
+            for nxt, cost in edges[cur]:
+                if dist[nxt] > dist[cur] + cost:
+                    dist[nxt] = dist[cur] + cost
+    for cur in range(1, n + 1):
+        if dist[cur] == INF: continue
+        for nxt, cost in edges[cur]:
+            if dist[nxt] > dist[cur] + cost:
+                return -1
     return '\n'.join(map(lambda x: str(x) if x != INF else '-1', dist[2:]))
 
 
 def main():
     n, m = map(int, input().split())
-    edges = [tuple(map(int, input().split())) for _ in range(m)]
+    edges = [[] for _ in range(n + 1)]
+    for _ in range(m):
+        a, b, c = map(int, input().split())
+        edges[a].append((b, c))
     print(bellman_ford(n, edges))
 
 
