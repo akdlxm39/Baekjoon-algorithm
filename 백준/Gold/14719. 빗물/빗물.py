6401687e81f2027ce6b61598b89c70d4688ac03a
Baek Jiung <zxcwldnd@gmail.com>
2025-09-01 13:56:06 +0900

    [Gold V] Title: 빗물, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -6,17 +6,17 @@ input = sys.stdin.readline
 def main():
     h, w = map(int, input().split())
     buildings = list(map(int, input().split()))
-    stack = []
+    left_max = [0] * w
+    right_max = [0] * w
+    left_max[0] = buildings[0]
+    right_max[-1] = buildings[-1]
+    for i in range(1, w):
+        left_max[i] = max(left_max[i - 1], buildings[i])
+    for i in range(w - 2, -1, -1):
+        right_max[i] = max(right_max[i + 1], buildings[i])
     ans = 0
     for i in range(w):
-        while len(stack) > 1 and buildings[stack[-1]] <= buildings[i]:
-            h_gap = min(buildings[stack[-2]], buildings[i]) - buildings[stack[-1]]
-            w_gap = i - stack[-2] - 1
-            ans += h_gap * w_gap
-            stack.pop()
-        if len(stack) == 1 and buildings[stack[-1]] <= buildings[i]:
-            stack.pop()
-        stack.append(i)
+        ans += min(left_max[i], right_max[i]) - buildings[i]
     print(ans)
 
 
