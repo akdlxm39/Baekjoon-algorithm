b3ddd68e8063e80813f0f2a717699b4fc340c224
Baek Jiung <zxcwldnd@gmail.com>
2025-09-07 16:19:13 +0900

    [Gold V] Title: 신기한 소수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.py
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.py
@@ -3,42 +3,28 @@ import sys
 input = sys.stdin.readline
 
 
-def get_primes(n):
-    primes = [False, False] + [True] * (n - 1)
-    for i in range(2, int(n ** 0.5) + 1):
-        if primes[i]:
-            primes[i + i::i] = [False] * (n // i - 1)
-    return primes
-
-
 def is_prime(n):
+    if n < 2:
+        return False
     for i in range(2, int(n ** 0.5) + 1):
         if n % i == 0:
             return False
     return True
 
 
-def dfs(n, primes, cur, cnt):
+def dfs(n, cur, cnt):
     if cnt == n:
         print(cur)
         return
-    elif cnt >= 5:
-        for i in range(1, 10):
-            nxt = cur * 10 + i
-            if is_prime(nxt):
-                dfs(n, primes, nxt, cnt + 1)
-        return
-    else:
-        for i in range(1, 10):
-            nxt = cur * 10 + i
-            if primes[nxt]:
-                dfs(n, primes, nxt, cnt + 1)
+    for i in range(1, 10):
+        nxt = cur * 10 + i
+        if is_prime(nxt):
+            dfs(n, nxt, cnt + 1)
 
 
 def main():
     n = int(input())
-    primes = get_primes(99999)
-    dfs(n, primes, 0, 0)
+    dfs(n, 0, 0)
 
 
 if __name__ == "__main__":
