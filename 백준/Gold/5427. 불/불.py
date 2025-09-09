36d28fb7b0f4000d85f0085bd71243ff16a8b938
Baek Jiung <zxcwldnd@gmail.com>
2025-09-09 17:14:38 +0900

    [Gold IV] Title: 불, Time: 1532 ms, Memory: 73528 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.py
+++ 백준/Gold/5427. 불/불.py
@@ -4,37 +4,30 @@ input = sys.stdin.readline
 dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
 
-def bfs(w, h, map_, fire, man):
-    burned = [[False] * w for _ in range(h)]
-    visited = [[False] * w for _ in range(h)]
-    fire_queue = fire
-    man_queue = [man]
+def bfs(w, h, map_, fire, mx, my):
+    visited = [[0] * w for _ in range(h)]
+    queue = []
     for x, y in fire:
-        burned[x][y] = True
-    visited[man[0]][man[1]] = True
+        queue.append((2, x, y))
+        visited[x][y] = 2
+    queue.append((1, mx, my))
+    visited[mx][my] = 1
     count = 0
-    while man_queue:
+    while queue and queue[-1][0] == 1:
         count += 1
-        nxt_fire_queue = []
-        for fx, fy in fire_queue:
+        nxt_queue = []
+        for cur, x, y in queue:
             for dx, dy in dirs:
-                fnx, fny = fx + dx, fy + dy
-                if not (0 <= fnx < h and 0 <= fny < w) or map_[fnx][fny] == '#' or burned[fnx][fny]:
+                nx, ny = x + dx, y + dy
+                if not (0 <= nx < h and 0 <= ny < w):
+                    if cur == 1:
+                        return count
                     continue
-                burned[fnx][fny] = True
-                nxt_fire_queue.append((fnx, fny))
-        fire_queue = nxt_fire_queue
-        nxt_man_queue = []
-        for mx, my in man_queue:
-            for dx, dy in dirs:
-                mnx, mny = mx + dx, my + dy
-                if not (0 <= mnx < h and 0 <= mny < w):
-                    return count
-                if map_[mnx][mny] == '#' or visited[mnx][mny] or burned[mnx][mny]:
+                if map_[nx][ny] == '#' or visited[nx][ny] >= cur:
                     continue
-                visited[mnx][mny] = True
-                nxt_man_queue.append((mnx, mny))
-        man_queue = nxt_man_queue
+                visited[nx][ny] = cur
+                nxt_queue.append((cur, nx, ny))
+        queue = nxt_queue
     return "IMPOSSIBLE"
 
 
@@ -44,6 +37,7 @@ def main():
         w, h = map(int, input().split())
         map_ = []
         fire = []
+        x = y = 0
         for i in range(h):
             line = input().rstrip()
             map_.append(line)
@@ -51,8 +45,8 @@ def main():
                 if line[j] == '*':
                     fire.append((i, j))
                 elif line[j] == '@':
-                    man = (i, j)
-        print(bfs(w, h, map_, fire, man))
+                    x, y = i, j
+        print(bfs(w, h, map_, fire, x, y))
 
 
 if __name__ == "__main__":
