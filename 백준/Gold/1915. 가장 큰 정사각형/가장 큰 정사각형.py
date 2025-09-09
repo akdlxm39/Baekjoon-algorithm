003e03724317249e82e1fcbc57eb2005705ef729
Baek Jiung <zxcwldnd@gmail.com>
2025-09-09 09:51:18 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 504 ms, Memory: 66160 KB -BaekjoonHub

---
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.py
@@ -0,0 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def main():
+    n, m = map(int, input().split())
+    nums = [list(map(int, list(input().rstrip()))) for _ in range(n)]
+    dp = [[0] * m for _ in range(n)]
+    ans = 0
+    for i in range(n):
+        for j in range(m):
+            if nums[i][j] == 0:
+                continue
+            size = min(dp[i][j - 1], dp[i - 1][j])
+            for k in range(size, 0, -1):
+                if nums[i - k][j - k]:
+                    dp[i][j] = k + 1
+                    break
+            else:
+                dp[i][j] = 1
+            ans = max(ans, dp[i][j])
+    print(ans * ans)
+
+
+if __name__ == "__main__":
+    main()
