1829d97de707c05b137087b50056b6ac3bbb0702
Baek Jiung <zxcwldnd@gmail.com>
2025-10-21 15:15:16 +0900

    [Gold V] Title: 1로 만들기 2, Time: 4 ms, Memory: 5932 KB -BaekjoonHub

--- C++17/백준/Gold/12852. 1로 만들기 2/1로 만들기 2.cc
+++ C++17/백준/Gold/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -1,3 +1,4 @@
+// https://testcase.ac/problems/12852 1로 만들기 2
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -43,12 +44,44 @@ void solve() {
     cout << '\n';
 }
 
+int dfs(int n) {
+    if (n <= 1)
+        return 0;
+    int a = dfs(n / 3) + n % 3;
+    int b = dfs(n / 2) + n % 2;
+    if (a <= b) {
+        dp[n] = n / 3;
+        return a + 1;
+    } else {
+        dp[n] = n / 2;
+        return b + 1;
+    }
+}
+
+void solve2() {
+    memset(dp, -1, sizeof(dp));
+    cin >> n;
+    int cnt = dfs(n);
+    dp[1] = 0;
+    cout << cnt << '\n';
+    int nxt = dp[n];
+    while (n) {
+        cout << n << ' ';
+        if (nxt * 3 != n && nxt * 2 != n) {
+            n--;
+            continue;
+        }
+        n = nxt;
+        nxt = dp[n];
+    }
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int T = 1;
     for (int t = 0; t < T; t++) {
-        solve();
+        solve2();
     }
     return 0;
 }
