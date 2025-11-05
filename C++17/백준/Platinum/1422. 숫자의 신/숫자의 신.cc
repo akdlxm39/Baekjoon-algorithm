1c538e46f7b877427dc5ddb066908f5a9f9be7ec
Baek Jiung <zxcwldnd@gmail.com>
2025-11-05 17:14:38 +0900

    [Platinum IV] Title: 숫자의 신, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- C++17/백준/Platinum/1422. 숫자의 신/숫자의 신.cc
+++ C++17/백준/Platinum/1422. 숫자의 신/숫자의 신.cc
@@ -3,33 +3,30 @@
 using namespace std;
 
 int n, k;
-vector<pair<string, int> > nums;
+vector<string> nums;
 
-bool cmp(pair<string, int> l, pair<string, int> r) {
-    return l.first + r.first > r.first + l.first;
+bool cmp1(string &l, string &r) {
+    if (l.length() == r.length())
+        return l < r;
+    return l.length() < r.length();
+}
+
+bool cmp2(string &l, string &r) {
+    return l + r > r + l;
 }
 
 void solve() {
     cin >> k >> n;
-    int max_len = -1;
-    int max_idx = -1;
-    for (int i = 0; i < k; ++i) {
-        string x;
-        cin >> x;
-        nums.emplace_back(x, 1);
-        int len = x.length();
-        if (max_len < len) {
-            max_len = len;
-            max_idx = i;
-        } else if (max_len == len && nums[max_idx].first < x) {
-            max_idx = i;
-        }
-    }
-    nums[max_idx].second += n - k;
-    sort(nums.begin(), nums.end(), cmp);
-    string ans;
-    for (auto [x, cnt]: nums) {
-        while (cnt--) cout << x;
+    int x = n - k;
+    nums.resize(k);
+    for (string &s: nums) cin >> s;
+    sort(nums.begin(), nums.end(), cmp2);
+    auto max_iter = max_element(nums.begin(), nums.end(), cmp1);
+    for (auto iter = nums.begin(); iter != nums.end(); ++iter) {
+        cout << *iter;
+        if (iter == max_iter)
+            while (x--)
+                cout << *iter;
     }
     cout << '\n';
 }
