4e608eb69bd52710903c5153dcca651507f0ef24
Baek Jiung <zxcwldnd@gmail.com>
2025-12-06 22:00:51 +0900

    [Gold IV] Title: 팰린드롬?, Time: 160 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -3,26 +3,28 @@
 using namespace std;
 
 int n, m, s, e;
-vector<int> nums;
-vector<vector<bool> > isPalindrome;
+vector<int> nums, palindromeRadius;
 
 void solve() {
     cin >> n;
-    nums.resize(n);
-    for (auto &x: nums)
-        cin >> x;
-    isPalindrome.resize(n, vector<bool>(n, true));
-    for (int k = 1; k < n; ++k) {
-        for (int i = 0; i < n - k; ++i) {
-            int j = i + k;
-            isPalindrome[i][j] = nums[i] == nums[j] && isPalindrome[i + 1][j - 1];
-        }
+    nums.resize(n * 2 + 1, 0);
+    for (int i = 0; i < n; ++i)
+        cin >> nums[i * 2 + 1];
+    nums[0] = -1, nums[n * 2] = -2;
+    palindromeRadius.resize(n * 2 + 1, 0);
+    for (int i = 1, c = 0, r = 0; i < 2 * n; ++i) {
+        if (i < r)
+            palindromeRadius[i] = min(r - i, palindromeRadius[2 * c - i]);
+        while (nums[i - (palindromeRadius[i] + 1)] == nums[i + (palindromeRadius[i] + 1)])
+            ++palindromeRadius[i];
+        if (i + palindromeRadius[i] > r)
+            c = i, r = i + palindromeRadius[i];
     }
-
     cin >> m;
     while (m--) {
         cin >> s >> e;
-        cout << (isPalindrome[s - 1][e - 1] ? "1\n" : "0\n");
+        int c = s + e - 1;
+        cout << (palindromeRadius[c] >= e - s ? "1\n" : "0\n");
     }
 }
 
