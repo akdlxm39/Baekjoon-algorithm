469e83a77ea073a9b4fcbfccaeffb570659aebc6
Baek Jiung <zxcwldnd@gmail.com>
2025-12-20 15:55:44 +0900

    [Gold V] Title: 다각형의 면적, Time: 4 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/2166. 다각형의 면적/다각형의 면적.cc
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.cc
@@ -1,55 +1,28 @@
-#include <bits/stdc++.h>
+// https://www.acmicpc.net/problem/2166 다각형의 면적
+#include<bits/stdc++.h>
 using namespace std;
 
-struct point {
-	double x, y;
-};
+int n;
+vector<array<double, 2> > points;
 
-struct line {
-	point A, B;
-	point v;
-	double len;
-	line(point A, point B) {
-		this->A = A;
-		this->B = B;
-		make_v();
-		len = sqrt(v.x * v.x + v.y * v.y);
-	}
-	void make_v() {
-		v.x = A.x - B.x;
-		v.y = A.y - B.y;
-	}
-};
-
-double cross_product(const line& L1, const line& L2) {
-	double res;
-	res = L1.v.x * L2.v.y - L1.v.y * L2.v.x;
-	return res;
+void solve() {
+    cin >> n;
+    points.resize(n);
+    for (auto &[x, y]: points)
+        cin >> x >> y;
+    double ans = 0.0;
+    for (int cur = 0, prev = n - 1; cur < n; prev = cur++) {
+        ans += (points[prev][0] + points[cur][0]) * (points[prev][1] - points[cur][1]);
+    }
+    cout << abs(ans) / 2 << '\n';
 }
 
-int N;
-point P[10001];
-double area;
-
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
-
-	cout.precision(1);
-	cout << fixed;
-
-	cin >> N;
-	for (int i = 1; i <= N; i++)
-		cin >> P[i].x >> P[i].y;
-	P[0].x = P[0].y = 0;
-
-	for (int i = 1; i < N; i++) {
-		line L1(P[0], P[i]), L2(P[0], P[i + 1]);
-		area += cross_product(L1, L2) / 2;
-	}
-	area += cross_product(line(P[0], P[N]), line(P[0], P[1])) / 2;
-
-	cout << abs(area) << '\n';
-
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout << fixed;
+    cout.precision(1);
+    int t = 1;
+    while (t--) solve();
+    return 0;
 }
