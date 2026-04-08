de975131a1048b7f606d9240c0e91e37b15c4baf
Baek Jiung <zxcwldnd@gmail.com>
2026-04-08 23:33:42 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 4 ms, Memory: 396 KB -BaekjoonHub

---
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+
+using namespace std;
+
+struct Point
+{
+    int y, x;
+    Point operator+(Point other) const;
+    bool isValid() const;
+    void visit() const;
+};
+
+const int SIZE = 16;
+const Point DIR[4] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+string map_[SIZE];
+Point start;
+int ans;
+
+inline Point Point::operator+(Point other) const
+{
+    return {y + other.y, x + other.x};
+}
+
+inline bool Point::isValid() const
+{
+    return 0 <= y && y < SIZE && 0 <= x && x < SIZE && map_[y][x] != '1';
+}
+
+inline void Point::visit() const
+{
+    map_[y][x] = '1';
+}
+
+bool dfs(Point cur)
+{
+    if (map_[cur.x][cur.y] == '3')
+        return true;
+    cur.visit();
+    for (Point d : DIR)
+    {
+        Point nxt = cur + d;
+        if (nxt.isValid() && dfs(nxt))
+            return true;
+    }
+    return false;
+}
+
+void init()
+{
+}
+
+void input()
+{
+    int dummy;
+    cin >> dummy;
+    for (int i = 0; i < SIZE; ++i)
+    {
+        cin >> map_[i];
+        size_t j = map_[i].find('2');
+        if (j != string::npos)
+            start.y = i, start.x = j;
+    }
+}
+
+void solve()
+{
+    ans = dfs(start);
+}
+
+void output()
+{
+    cout << ans << '\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 10;
+    for (int testCase = 1; testCase <= T; ++testCase)
+    {
+        init();
+        input();
+        solve();
+        cout << '#' << testCase << ' ';
+        output();
+    }
+
+    return 0;
+}
