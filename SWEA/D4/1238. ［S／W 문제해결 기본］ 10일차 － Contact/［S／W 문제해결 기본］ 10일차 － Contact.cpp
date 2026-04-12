49ab7e0791dd3767fc50a7e4581d4e7a8f6d5220
Baek Jiung <zxcwldnd@gmail.com>
2026-04-12 17:42:05 +0900

    [D4] Title: [S/W 문제해결 기본] 10일차 - Contact, Time: 5 ms, Memory: 412 KB -BaekjoonHub

--- SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.cpp
+++ SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.cpp
@@ -4,8 +4,6 @@
 
 using namespace std;
 
-constexpr int INF = int(1e9);
-
 bool adj_matrix[101][101];
 int dist[101];
 int start, ans;
@@ -15,20 +13,10 @@ int bfs()
     queue<int> q;
     q.push(start);
     dist[start] = 0;
-    int max_dist = -1, ret;
     while (!q.empty())
     {
         int cur = q.front();
         q.pop();
-        if (max_dist < dist[cur])
-        {
-            max_dist = dist[cur];
-            ret = cur;
-        }
-        else if (max_dist == dist[cur] && ret < cur)
-        {
-            ret = cur;
-        }
         for (int nxt = 1; nxt <= 100; ++nxt)
         {
             if (adj_matrix[cur][nxt] && dist[nxt] == -1)
@@ -36,6 +24,15 @@ int bfs()
                 q.push(nxt);
                 dist[nxt] = dist[cur] + 1;
             }
+        }
+    }
+    int max_dist = 0, ret = start;
+    for (int i = 100; i >= 1; --i)
+    {
+        if (max_dist < dist[i])
+        {
+            max_dist = dist[i];
+            ret = i;
         }
     }
     return ret;
