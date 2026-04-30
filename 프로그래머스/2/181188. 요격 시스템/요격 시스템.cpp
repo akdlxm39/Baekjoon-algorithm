87c3c088347a19230083b4ad2a20c6c0ec49487e
Baek Jiung <zxcwldnd@gmail.com>
2026-04-30 23:06:26 +0900

    [level 2] Title: 요격 시스템, Time: 124.78 ms, Memory: 96.4 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
@@ -7,17 +7,12 @@ using namespace std;
 int solution(vector<vector<int>> targets)
 {
     int answer = 0;
-    for (auto &target : targets)
-    {
-        target[0] = target[0] * 2 + 1;
-        target[1] = target[1] * 2 - 1;
-    }
     sort(targets.begin(), targets.end(),
          [](const vector<int> &a, const vector<int> &b) { return a[1] != b[1] ? a[1] < b[1] : a[0] < b[0]; });
     int cur = 0;
     for (const auto &target : targets)
     {
-        if (cur < target[0])
+        if (cur <= target[0])
         {
             cur = target[1];
             answer++;
